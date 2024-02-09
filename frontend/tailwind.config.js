5b97a7a7f20f34df4ab2fbf75da858cd06b96ba2
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-09 17:50:46 +0200

    LIME-4: + tailwind color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        colors: {
+            'green-pine': '#037768',
+            'red': '#F62D2D',
+            'purple': '#7306FF',
+            'magenta': '#DC40CD',
+            'yellow': '#B2CA0D',
+            'yellow-light': '#E0FE10',
+            'grey': '#64748B',
+            'grey-light': '#AEB4BB',
+            'gray-dark': '#383F4A',
+            'grey-blue': '#798392',
+            'grey-fiord': '#475569',
+            'black-pearl': '#1C2227',
+            'black-ebony': '#2A2F37',
+        },
         extend: {},
     },
     plugins: [],
