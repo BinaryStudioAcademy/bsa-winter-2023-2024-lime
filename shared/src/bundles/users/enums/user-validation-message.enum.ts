b80d4318983c5299bd2102d531299236fcc7e043
JKaypa <jose_kaypa@hotmail.com>
2024-02-10 11:23:58 -0500

    LIME-6: + password required length message

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -1,6 +1,7 @@
 enum UserValidationMessage {
     EMAIL_REQUIRE = 'Email is required',
     EMAIL_WRONG = 'Email is wrong',
+    PASSWORD_REQUIRED = 'Password must have at least 8 characters',
 }
 
 export { UserValidationMessage };
