f77da89b5e130ab090d8a0f4e1bcaffcb395bbed
stpalamar <stanislav6121@gmail.com>
2024-02-12 20:15:25 +0200

    LIME-7: + import radio component

--- frontend/src/bundles/common/components/components.ts
+++ frontend/src/bundles/common/components/components.ts
@@ -1,6 +1,7 @@
 export { Button } from './button/button.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
+export { Radio } from './radio/radio.js';
 export { RouterProvider } from './router-provider/router-provider.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
