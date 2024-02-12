e09767eac921ebc8dc6db74676be2eba7533da78
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-12 17:06:44 +0200

    LIME-5: + added export to components file

--- frontend/src/bundles/common/components/components.ts
+++ frontend/src/bundles/common/components/components.ts
@@ -2,5 +2,6 @@ export { Button } from './button/button.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
 export { RouterProvider } from './router-provider/router-provider.js';
+export { Select } from './select/select.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
