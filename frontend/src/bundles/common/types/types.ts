1e6f02ae099146d1f9ce6b31f55aa6564d04fc99
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-12 15:14:01 +0200

    LIME-4: * fixed button types

--- frontend/src/bundles/common/types/types.ts
+++ frontend/src/bundles/common/types/types.ts
@@ -1,10 +1,5 @@
 export { type AsyncThunkConfig } from './async-thunk-config.type.js';
 export {
-    type ButtonProperties,
-    ButtonSize,
-    ButtonVariant,
-} from './button.type.js';
-export {
     type ServerErrorDetail,
     type ServerErrorResponse,
     type ValidationSchema,
