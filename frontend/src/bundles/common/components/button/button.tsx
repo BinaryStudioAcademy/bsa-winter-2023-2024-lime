fbaa7751a0071ada5e6b5863fceffd4ad89611fb
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-12 15:40:44 +0200

    LIME-4: * fixed button styled changed

--- frontend/src/bundles/common/components/button/button.tsx
+++ frontend/src/bundles/common/components/button/button.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from 'react';
+import { type ValueOf } from 'shared';
 
 import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
 
@@ -16,8 +17,8 @@ const ButtonSize = {
 type ButtonType = 'button' | 'submit';
 
 type ButtonProperties = {
-    size: (typeof ButtonSize)[keyof typeof ButtonSize];
-    variant: (typeof ButtonVariant)[keyof typeof ButtonVariant];
+    size: ValueOf<typeof ButtonSize>;
+    variant: ValueOf<typeof ButtonVariant>;
     label: string;
     leftIcon?: ReactNode;
     rightIcon?: ReactNode;
@@ -27,16 +28,23 @@ type ButtonProperties = {
     onClick?: () => void;
 };
 
-const buttonVariantToClasses: Record<
-    (typeof ButtonVariant)[keyof typeof ButtonVariant],
-    string
-> = {
+const baseClasses =
+    'w-full flex justify-center items-center transition ease-in-out duration-300';
+
+const buttonVariantToClasses: Record<ValueOf<typeof ButtonVariant>, string> = {
     [ButtonVariant.PRIMARY]:
         'rounded-lg bg-lm-yellow-100 text-lm-black-300 hover:bg-lm-yellow-200 disabled:text-lm-grey-200 disabled:bg-lm-grey-300',
     [ButtonVariant.SECONDARY]:
         'border border-lm-yellow-100 rounded-lg bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 hover:border-lm-yellow-200 disabled:text-lm-grey-300 disabled:border-lm-grey-300',
     [ButtonVariant.TERTIARY]:
         'bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 disabled:text-lm-grey-300',
+};
+
+const buttonSizesToClasses: Record<ValueOf<typeof ButtonSize>, string> = {
+    [ButtonSize.SMALL]:
+        'px-4 py-2 h-[32px] text-[14px] font-bold leading-[1.3rem] gap-[4px]',
+    [ButtonSize.MEDIUM]:
+        'px-6 py-4 h-[54px] text-[16px] font-bold leading-[1.3rem] gap-[8px]',
 };
 
 const Button: React.FC<ButtonProperties> = ({
@@ -50,22 +58,15 @@ const Button: React.FC<ButtonProperties> = ({
     isDisabled = false,
     ...properties
 }: ButtonProperties): JSX.Element => {
-    const baseClasses =
-        'flex justify-center items-center transition ease-in-out duration-300';
-    const sizeClasses =
-        size === ButtonSize.SMALL
-            ? 'px-4 py-2 h-[32px] text-[14px] font-bold leading-[1.3rem] gap-[4px]'
-            : 'px-6 py-4 h-[54px] text-[16px] font-bold leading-[1.3rem] gap-[8px]';
     return (
         <button
             disabled={isDisabled}
             type={type}
             className={getValidClassNames(
                 baseClasses,
-                'w-full',
-                sizeClasses,
+                buttonSizesToClasses[size],
                 buttonVariantToClasses[variant],
-                className as string,
+                className,
             )}
             {...properties}
         >
