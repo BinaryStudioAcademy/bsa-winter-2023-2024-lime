ad8b648472770ddafbd165b2d064db38de17278b
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-12 15:21:45 +0200

    LIME-4: * fixed button split this into maps

--- frontend/src/bundles/common/components/button/button.tsx
+++ frontend/src/bundles/common/components/button/button.tsx
@@ -27,21 +27,16 @@ type ButtonProperties = {
     onClick?: () => void;
 };
 
-const classes = {
-    base: 'flex justify-center items-center transition ease-in-out duration-300',
-    size: {
-        small: 'px-4 py-2 h-[32px] text-[14px] font-bold leading-[1.3rem] gap-[4px]',
-        medium: 'px-6 py-4 h-[54px] text-[16px] font-bold leading-[1.3rem] gap-[8px]',
-    },
-
-    variant: {
-        primary:
-            'rounded-lg bg-lm-yellow-100 text-lm-black-300 hover:bg-lm-yellow-200 disabled:text-lm-grey-200 disabled:bg-lm-grey-300',
-        secondary:
-            'border border-lm-yellow-100 rounded-lg bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 hover:border-lm-yellow-200 disabled:text-lm-grey-300 disabled:border-lm-grey-300',
-        tertiary:
-            'bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 disabled:text-lm-grey-300',
-    },
+const buttonVariantToClasses: Record<
+    (typeof ButtonVariant)[keyof typeof ButtonVariant],
+    string
+> = {
+    [ButtonVariant.PRIMARY]:
+        'rounded-lg bg-lm-yellow-100 text-lm-black-300 hover:bg-lm-yellow-200 disabled:text-lm-grey-200 disabled:bg-lm-grey-300',
+    [ButtonVariant.SECONDARY]:
+        'border border-lm-yellow-100 rounded-lg bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 hover:border-lm-yellow-200 disabled:text-lm-grey-300 disabled:border-lm-grey-300',
+    [ButtonVariant.TERTIARY]:
+        'bg-transparent text-lm-yellow-100 hover:text-lm-yellow-200 disabled:text-lm-grey-300',
 };
 
 const Button: React.FC<ButtonProperties> = ({
@@ -55,14 +50,21 @@ const Button: React.FC<ButtonProperties> = ({
     isDisabled = false,
     ...properties
 }: ButtonProperties): JSX.Element => {
+    const baseClasses =
+        'flex justify-center items-center transition ease-in-out duration-300';
+    const sizeClasses =
+        size === ButtonSize.SMALL
+            ? 'px-4 py-2 h-[32px] text-[14px] font-bold leading-[1.3rem] gap-[4px]'
+            : 'px-6 py-4 h-[54px] text-[16px] font-bold leading-[1.3rem] gap-[8px]';
     return (
         <button
             disabled={isDisabled}
             type={type}
             className={getValidClassNames(
-                classes.base,
-                classes.size[size],
-                classes.variant[variant],
+                baseClasses,
+                'w-full',
+                sizeClasses,
+                buttonVariantToClasses[variant],
                 className as string,
             )}
             {...properties}
