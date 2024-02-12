fbc835ff3332409a2eed81b0736bfc2c1a86b1cf
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-02-12 18:21:56 +0200

    LIME-10: * resolve merge conflict

--- frontend/src/bundles/common/types/types.ts
+++ frontend/src/bundles/common/types/types.ts
@@ -1,9 +1,4 @@
 export { type AsyncThunkConfig } from './async-thunk-config.type.js';
-export {
-    type ButtonProperties,
-    ButtonSize,
-    ButtonVariant,
-} from './button.type.js';
 export { type Color, type Size } from './styling/styling.js';
 export {
     type ServerErrorDetail,
