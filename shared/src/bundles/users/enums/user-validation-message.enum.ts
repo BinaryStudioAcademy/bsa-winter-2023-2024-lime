cf8e14eb028ca7f8ea26b063b7892d1e5f98647a
JKaypa <jose_kaypa@hotmail.com>
2024-02-13 11:56:41 -0500

    LIME-15: + enum validation messages

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -1,6 +1,8 @@
 enum UserValidationMessage {
-    EMAIL_REQUIRE = 'Email is required',
-    EMAIL_WRONG = 'Email is wrong',
+    EMAIL_REQUIRE = 'Please fill out this field',
+    INVALID_EMAIL = 'Please enter a valid email',
+    INVALID_PASSWORD = 'Password must have from 6 to 12 characters',
+    PASSWORD_MISMATCH = 'Passwords must be identical. Please try again',
 }
 
 export { UserValidationMessage };
