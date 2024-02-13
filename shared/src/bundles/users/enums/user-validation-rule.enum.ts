cac9d7cbc3faafc3f0ded731d873f33a79503077
JKaypa <jose_kaypa@hotmail.com>
2024-02-13 11:58:51 -0500

    LIME-15: + const validation rules

--- shared/src/bundles/users/enums/user-validation-rule.enum.ts
+++ shared/src/bundles/users/enums/user-validation-rule.enum.ts
@@ -1,5 +1,9 @@
 const UserValidationRule = {
-    EMAIL_MINIMUM_LENGTH: 1,
+    EMAIL_REQUIRED: 1,
+    EMAIL_MIN_LENGTH: 6,
+    EMAIL_MAX_LENGTH: 320,
+    PASSWORD_MIN_LENGTH: 6,
+    PASSWORD_MAX_LENGTH: 12,
 } as const;
 
 export { UserValidationRule };
