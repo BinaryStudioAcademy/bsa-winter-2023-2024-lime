d1dd2029f1f5c0d7ace5ee8e5100e978d292267a
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-13 20:51:55 +0200

    LIME-41: + layout component

--- frontend/src/bundles/common/components/layout/layout.tsx
+++ frontend/src/bundles/common/components/layout/layout.tsx
@@ -8,12 +8,7 @@ type Properties = {
 };
 
 const Layout = ({ children, className }: Properties): JSX.Element => (
-    <div
-        className={getValidClassNames(
-            className,
-            'mx-auto my-0 w-full px-8 py-6',
-        )}
-    >
+    <div className={getValidClassNames(className, 'mx-auto my-0 w-full p-6')}>
         {children}
     </div>
 );
