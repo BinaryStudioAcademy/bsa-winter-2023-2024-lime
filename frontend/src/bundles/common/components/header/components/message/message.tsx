619c3bf767cdb2a841ab3ad82fd8045dad6ead79
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-13 20:39:00 +0200

    LIME-41: + message block

---
+++ frontend/src/bundles/common/components/header/components/message/message.tsx
@@ -0,0 +1,12 @@
+const Message = (): JSX.Element => {
+    return (
+        <div className="flex flex-col gap-0.5">
+            <p className="text-lm-grey-300 text-[0.625rem] font-normal">
+                Good Morning
+            </p>
+            <p className="h5 text-lm-grey-300 font-bold">Welcome Back!</p>
+        </div>
+    );
+};
+
+export { Message };
