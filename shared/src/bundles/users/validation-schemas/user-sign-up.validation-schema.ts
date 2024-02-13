b516eeca3862562fd42b1ee593ccd898c9071342
JKaypa <jose_kaypa@hotmail.com>
2024-02-13 16:27:02 -0500

    LIME-15: * UserSignUpRequestValidationDto as it was originally

--- shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/user-sign-up.validation-schema.ts
@@ -5,6 +5,7 @@ import { UserValidationMessage, UserValidationRule } from '../enums/enums.js';
 type UserSignUpRequestValidationDto = {
     email: z.ZodString;
     password: z.ZodString;
+    passwordConfirm: z.ZodString;
 };
 
 const userSignUp = z
@@ -12,14 +13,59 @@ const userSignUp = z
         email: z
             .string()
             .trim()
-            .min(UserValidationRule.EMAIL_MINIMUM_LENGTH, {
+            .min(UserValidationRule.EMAIL_REQUIRED, {
                 message: UserValidationMessage.EMAIL_REQUIRE,
             })
+            .min(UserValidationRule.EMAIL_MIN_LENGTH, {
+                message: UserValidationMessage.INVALID_EMAIL,
+            })
+            .max(UserValidationRule.EMAIL_MAX_LENGTH, {
+                message: UserValidationMessage.INVALID_EMAIL,
+            })
             .email({
-                message: UserValidationMessage.EMAIL_WRONG,
+                message: UserValidationMessage.INVALID_EMAIL,
             }),
-        password: z.string().trim(),
+
+        password: z
+            .string()
+            .trim()
+            .min(UserValidationRule.PASSWORD_MIN_LENGTH, {
+                message: UserValidationMessage.INVALID_PASSWORD,
+            })
+            .max(UserValidationRule.PASSWORD_MAX_LENGTH, {
+                message: UserValidationMessage.INVALID_PASSWORD,
+            }),
+
+        passwordConfirm: z
+            .string()
+            .trim()
+            .min(UserValidationRule.PASSWORD_MIN_LENGTH, {
+                message: UserValidationMessage.INVALID_PASSWORD,
+            })
+            .max(UserValidationRule.PASSWORD_MAX_LENGTH, {
+                message: UserValidationMessage.INVALID_PASSWORD,
+            }),
     })
-    .required();
+    .refine(
+        (value) => {
+            const [local, domain] = value.email.split('@');
+            return (
+                local &&
+                local.length > 0 &&
+                local.length < 65 &&
+                domain &&
+                domain.length > 3 &&
+                domain.length < 255
+            );
+        },
+        {
+            message: UserValidationMessage.INVALID_EMAIL,
+            path: ['email'],
+        },
+    )
+    .refine((value) => value.password === value.passwordConfirm, {
+        message: UserValidationMessage.PASSWORD_MISMATCH,
+        path: ['passwordConfirm'],
+    });
 
 export { userSignUp };
