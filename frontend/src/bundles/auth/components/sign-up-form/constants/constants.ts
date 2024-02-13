c0f164665b204a4fa7df6d8cb9969493cf8b72ef
JKaypa <jose_kaypa@hotmail.com>
2024-02-13 11:17:50 -0500

    LIME-15: + password confirm to default values constant

--- frontend/src/bundles/auth/components/sign-up-form/constants/constants.ts
+++ frontend/src/bundles/auth/components/sign-up-form/constants/constants.ts
@@ -3,6 +3,7 @@ import { type UserSignUpRequestDto } from '~/bundles/users/users.js';
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
     email: '',
     password: '',
+    passwordConfirm: '',
 };
 
 export { DEFAULT_SIGN_UP_PAYLOAD };
