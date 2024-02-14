5ae4db7795437e48f5c1208675ed7b3871c79770
David <davidcharif@hotmail.com>
2024-02-13 23:43:56 -0500

    LIME-42: + Added state to manage the pathname and highlight on route

--- frontend/src/bundles/common/components/sidebar/sidebar.tsx
+++ frontend/src/bundles/common/components/sidebar/sidebar.tsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '~/assets/img/logo.svg';
 
 import { AppRoute } from '../../enums/app-route.enum.js';
 import {
     GoalsIcon,
+    HelpIcon,
+    LogoutIcon,
     OverviewIcon,
     ScheduleIcon,
     WorkoutIcon,
@@ -12,49 +15,64 @@ import {
 import { SidebarNav } from '../sidebar-nav/sidebar-nav.js';
 
 const Sidebar: React.FC = () => {
+    const { pathname } = useLocation(); // Get the current pathname
+
+    // Example usage of useState to track active state for each route
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [activeRoute, setActiveRoute] = useState(pathname);
     return (
         <div className="bg-lm-black-100 flex h-screen w-72 flex-col content-center items-center p-7 text-white">
-            <div className='flex w-full mb-5'>
+            <div className="mb-5 flex w-full">
                 <Link to={'/'}>
                     <img
                         src={logo}
                         alt="Logo"
-                        className='w-40
-                        '
+                        className="w-40
+                        "
                     />
                 </Link>
             </div>
-            <div className="inner h-3/4 w-full border-b border-gray-700 ">
+            <div className="inner h-3/4 w-full border-gray-700 ">
                 <div className="flex flex-col gap-4">
                     <SidebarNav
                         icon={<OverviewIcon />}
                         text="Overview"
                         to={AppRoute.OVERVIEW}
+                        isActive={activeRoute === AppRoute.OVERVIEW}
                     />
                     <SidebarNav
                         icon={<WorkoutIcon />}
                         text="Workout"
                         to={AppRoute.WORKOUT}
+                        isActive={activeRoute === AppRoute.WORKOUT}
                     />
                     <SidebarNav
                         icon={<GoalsIcon />}
                         text="Goals"
                         to={AppRoute.GOALS}
+                        isActive={activeRoute === AppRoute.GOALS}
                     />
                     <SidebarNav
                         icon={<ScheduleIcon />}
                         text="Schedule"
                         to={AppRoute.SCHEDULE}
+                        isActive={activeRoute === AppRoute.SCHEDULE}
                     />
                 </div>
             </div>
 
-            {/* Bottom rows */}
-            <div className="h-1/4 w-full px-6 py-4">
-                <div className="mb-4 text-lg font-semibold">Bottom Rows</div>
-                <div className="flex flex-col">
-                    <div className="py-2">Item 5</div>
-                    <div className="py-2">Item 6</div>
+            <div className="h-1/4 w-full flex">
+                <div className="flex flex-col justify-center w-full gap-3">
+                    <SidebarNav
+                        icon={<HelpIcon />}
+                        text="Help"
+                        to={AppRoute.GOALS}
+                    />
+                    <SidebarNav
+                        icon={<LogoutIcon />}
+                        text="Logout"
+                        to={AppRoute.SCHEDULE}
+                    />
                 </div>
             </div>
         </div>
