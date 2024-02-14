ccd0c90da0089cc785401a645a52764d50dd2bc2
jhladkov <zhenua.gladkovv@gmail.com>
2024-02-14 14:47:40 +0200

    LIME-12: * fastify-types.d.ts

--- backend/fastify-types.d.ts
+++ backend/fastify-types.d.ts
@@ -1,7 +1,8 @@
 import { type FastifyRequest as OriginalFastifyRequest } from 'fastify';
+import { type JWTPayload } from 'jose';
 
 declare module 'fastify' {
     interface FastifyRequest extends OriginalFastifyRequest {
-        user?: string | object;
+        user?: JWTPayload | null;
     }
 }
