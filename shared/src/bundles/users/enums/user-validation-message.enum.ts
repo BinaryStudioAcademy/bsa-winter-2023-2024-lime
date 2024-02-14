570b75d6de95b725bf07ab41fe297daf13f785a1
JKaypa <jose_kaypa@hotmail.com>
2024-02-14 16:03:47 -0500

    LIME-15: * refactor validation rules and messages

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -1,8 +1,14 @@
-enum UserValidationMessage {
-    EMAIL_REQUIRE = 'Please fill out this field',
-    INVALID_EMAIL = 'Please enter a valid email',
-    INVALID_PASSWORD = 'Password must have from 6 to 12 characters',
-    PASSWORD_MISMATCH = 'Passwords must be identical. Please try again',
-}
+import { UserValidationRule } from './user-validation-rule.enum.js';
+
+const { PASSWORD } = UserValidationRule;
+
+const UserValidationMessage = {
+    REQUIRED: 'Please fill out this field',
+    INVALID_EMAIL: 'Please enter a valid email',
+    INVALID_PASSWORD: `Password must have from ${PASSWORD.MIN_LENGTH} to ${PASSWORD.MAX_LENGTH} characters`,
+    PASSWORD_MISMATCH: 'Passwords must be identical. Please try again',
+    EMAIL_ALREADY_TAKEN:
+        'User with this email already exists. Log in if it is you',
+} as const;
 
 export { UserValidationMessage };
