d650fc608bc5b9b1811a9e43273f2d8aca9c0764
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-02-14 20:33:22 +0200

    LIME-36: * backend common types

--- backend/src/common/types/types.ts
+++ backend/src/common/types/types.ts
@@ -1,5 +1,5 @@
 export { type Entity } from './entity.type.js';
-export { type JwtPayload } from './jwt.type.js';
+export { type JwtPayloadOptions } from './jwt.type.js';
 export { type Repository } from './repository.type.js';
 export { type Service } from './service.type.js';
 export {
