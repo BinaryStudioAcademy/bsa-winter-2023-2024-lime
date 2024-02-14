1bcfa782e4ba86c13cc987951f0bac7464285b5a
valery-chumak <blossssom88@gmail.com>
2024-02-14 13:51:36 +0200

    LIME-11: * edit type

--- frontend/src/bundles/common/components/components.ts
+++ frontend/src/bundles/common/components/components.ts
@@ -1,6 +1,8 @@
 export { Button } from './button/button.js';
 export { Input } from './input/input.js';
 export { Link } from './link/link.js';
+export { ProtectedRoute } from './protected-route/protected-route.js';
 export { RouterProvider } from './router-provider/router-provider.js';
 export { Provider as StoreProvider } from 'react-redux';
 export { Outlet as RouterOutlet } from 'react-router-dom';
+export { Navigate } from 'react-router-dom';
