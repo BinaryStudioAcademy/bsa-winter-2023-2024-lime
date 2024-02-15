9d6b83da864901555ffbde232f8419c68ccb1ccd
David <davidcharif@hotmail.com>
2024-02-15 15:33:11 -0500

    LIME-42: + Handle open and close animation

--- frontend/src/bundles/common/components/sidebar/sidebar.tsx
+++ frontend/src/bundles/common/components/sidebar/sidebar.tsx
@@ -1,6 +1,11 @@
-import { useLocation, useState } from '~/bundles/common/hooks/hooks.js';
+import {
+    useEffect,
+    useLocation,
+    useState,
+} from '~/bundles/common/hooks/hooks.js';
 
 import { AppRoute } from '../../enums/app-route.enum.js';
+import { getValidClassNames } from '../../helpers/helpers.js';
 import {
     GoalsIcon,
     HelpIcon,
@@ -11,13 +16,37 @@ import {
 } from '../icons/sidebar-icons/sidebar-icons.js';
 import { SidebarNav } from '../sidebar-nav/sidebar-nav.js';
 
-const Sidebar: React.FC = () => {
+type Properties = {
+    isOpen: boolean;
+};
+
+const styles = {
+    baseStyle:
+        'bg-lm-black-100 flex h-[95vh] w-72 flex-col content-center items-center p-7 text-white',
+    animationStyle: 'transition-transform duration-[0.5s] ease-[ease-in-out]',
+};
+
+const Sidebar = ({ isOpen }: Properties): JSX.Element => {
     const { pathname } = useLocation();
 
     const [activeRoute] = useState(pathname);
 
+    const [sidebarStyle, setSidebarStyle] = useState({});
+
+    useEffect(() => {
+        isOpen
+            ? setSidebarStyle({ transform: 'translateX(0)' })
+            : setSidebarStyle({ transform: 'translateX(-100%)' });
+    }, [isOpen]);
+
     return (
-        <div className="bg-lm-black-100 flex h-[95vh] w-72 flex-col content-center items-center p-7 text-white">
+        <div
+            className={getValidClassNames(
+                styles.baseStyle,
+                styles.animationStyle,
+            )}
+            style={sidebarStyle}
+        >
             <div className="inner h-3/4 w-full border-gray-700 ">
                 <div className="flex flex-col gap-4">
                     <SidebarNav
