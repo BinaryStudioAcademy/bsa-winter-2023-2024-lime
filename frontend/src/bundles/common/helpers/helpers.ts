6dc741c466a24ff67847899e2db1af8a62753c67
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-15 13:35:52 +0200

    LIME-53: - removed unused format date helper

--- frontend/src/bundles/common/helpers/helpers.ts
+++ frontend/src/bundles/common/helpers/helpers.ts
@@ -1,3 +1,2 @@
-export { getFormatDate } from './get-format-date/get-format-date.js';
 export { getValidClassNames } from './get-valid-class-names/get-valid-class-names.js';
 export { configureString } from 'shared';
