0aedb2fcd5d3e8364c7c116a5c5903e5bf3d9e96
velisssa <tybinka.vasylyna@gmail.com>
2024-02-15 22:39:28 +0100

    LIME-47: * use icon from heroicons

--- frontend/src/bundles/common/components/modal/modal.tsx
+++ frontend/src/bundles/common/components/modal/modal.tsx
@@ -1,6 +1,5 @@
+import { XMarkIcon } from '@heroicons/react/16/solid';
 import { type ReactNode } from 'react';
-
-import iconClose from '~/assets/img/close.svg';
 
 type Properties = {
     isOpen: boolean;
@@ -20,12 +19,9 @@ const Modal: React.FC<Properties> = ({ isOpen, title, onClose, children }) => {
                 role="presentation"
             />
             <div className="bg-lm-black-100 rounded-34 fixed fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-start justify-center px-32 pb-24 pt-16 shadow-md transition-all ">
-                <img
-                    src={iconClose}
-                    alt="Close modal"
+                <XMarkIcon
                     onClick={onClose}
-                    className="absolute right-8 top-8 cursor-pointer"
-                    role="presentation"
+                    className="stroke-lm-grey-500 fill-lm-grey-500 hover:stroke-lm-yellow-100 hover:fill-lm-yellow-100 absolute right-8 top-8 h-3 w-3 cursor-pointer transition-all "
                 />
 
                 <h3 className="text-lm-grey-200 mb-10 text-xl font-extrabold leading-6">
