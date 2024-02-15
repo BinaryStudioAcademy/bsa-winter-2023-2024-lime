974b1daf8f6d541189dc4ccd297d03482b065208
JKaypa <jose_kaypa@hotmail.com>
2024-02-15 01:30:34 -0500

    LIME-15: * split validation regarding its purpose

--- shared/src/bundles/users/validation-schemas/user-auth.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/user-auth.validation-schema.ts
@@ -5,40 +5,61 @@ import { UserValidationMessage, UserValidationRule } from '../enums/enums.js';
 type UserAuthRequestValidationDto = {
     email: z.ZodString;
     password: z.ZodString;
-    passwordConfirm?: z.ZodString;
 };
 
-const userAuth = z
-    .object<UserAuthRequestValidationDto>({
-        email: z
-            .string()
-            .trim()
-            .min(1, {
-                message: UserValidationMessage.REQUIRED,
-            })
-            .min(UserValidationRule.EMAIL.MIN_LENGTH, {
-                message: UserValidationMessage.INVALID_EMAIL,
-            })
-            .max(UserValidationRule.EMAIL.MAX_LENGTH, {
-                message: UserValidationMessage.INVALID_EMAIL,
-            })
-            .email({
-                message: UserValidationMessage.INVALID_EMAIL,
-            }),
+const basicUserAuth = z.object<UserAuthRequestValidationDto>({
+    email: z
+        .string()
+        .trim()
+        .min(1, {
+            message: UserValidationMessage.REQUIRED,
+        })
+        .min(UserValidationRule.EMAIL.MIN_LENGTH, {
+            message: UserValidationMessage.INVALID_EMAIL,
+        })
+        .max(UserValidationRule.EMAIL.MAX_LENGTH, {
+            message: UserValidationMessage.INVALID_EMAIL,
+        })
+        .email({
+            message: UserValidationMessage.INVALID_EMAIL,
+        }),
 
-        password: z
-            .string()
-            .trim()
-            .min(1, {
-                message: UserValidationMessage.REQUIRED,
-            })
-            .min(UserValidationRule.PASSWORD.MIN_LENGTH, {
-                message: UserValidationMessage.INVALID_PASSWORD,
-            })
-            .max(UserValidationRule.PASSWORD.MAX_LENGTH, {
-                message: UserValidationMessage.INVALID_PASSWORD,
-            }),
+    password: z
+        .string()
+        .trim()
+        .min(1, {
+            message: UserValidationMessage.REQUIRED,
+        })
+        .min(UserValidationRule.PASSWORD.MIN_LENGTH, {
+            message: UserValidationMessage.INVALID_PASSWORD,
+        })
+        .max(UserValidationRule.PASSWORD.MAX_LENGTH, {
+            message: UserValidationMessage.INVALID_PASSWORD,
+        }),
+});
 
+const userAuth = basicUserAuth.refine(
+    (value) => {
+        const [local, domain] = value.email.split('@');
+        const { EMAIL } = UserValidationRule;
+
+        return (
+            local &&
+            local.length >= EMAIL.LOCAL_MIN_LENGTH &&
+            local.length <= EMAIL.LOCAL_MAX_LENGTH &&
+            domain &&
+            domain.length >= EMAIL.DOMAIN_MIN_LENGTH &&
+            domain.length <= EMAIL.DOMAIN_MAX_LENGTH
+        );
+    },
+    {
+        message: UserValidationMessage.INVALID_EMAIL,
+        path: ['email'],
+    },
+);
+
+const userAuthPWConfirm = basicUserAuth
+    .extend({
         passwordConfirm: z
             .string()
             .trim()
@@ -76,4 +97,4 @@ const userAuth = z
         path: ['passwordConfirm'],
     });
 
-export { userAuth };
+export { userAuth, userAuthPWConfirm };
