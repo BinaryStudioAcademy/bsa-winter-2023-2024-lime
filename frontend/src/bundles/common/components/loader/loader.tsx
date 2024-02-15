39559bd1e3d9f0abd9a67c190e5f1c7dc4a53b9e
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-15 14:39:45 +0200

    LIME-66: * loader component

--- frontend/src/bundles/common/components/loader/loader.tsx
+++ frontend/src/bundles/common/components/loader/loader.tsx
@@ -1,26 +1,29 @@
-import {
-    LogoIcon,
-    LogoIconColor,
-    LogoIconSize,
-} from '~/bundles/common/components/icons/icons.js';
 import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
 
+import { Icon } from '../components.js';
+import { IconColor, IconSize } from '../icon/enums/enums.js';
+
 type Properties = {
     isOverflow?: boolean;
-    color?: ValueOf<typeof LogoIconColor>;
-    size?: ValueOf<typeof LogoIconSize>;
+    color?: ValueOf<typeof IconColor>;
+    size?: ValueOf<typeof IconSize>;
 };
 
 const Loader: React.FC<Properties> = ({
     isOverflow,
-    color = LogoIconColor.PRIMARY,
-    size = LogoIconSize.LARGE,
+    color = IconColor.PRIMARY,
+    size = IconSize.LARGE,
 }: Properties): JSX.Element => {
     const spinner = (
         <>
             <span aria-hidden="true">
-                <LogoIcon color={color} size={size} className="animate-load" />
+                <Icon
+                    name="logoIcon"
+                    color={color}
+                    size={size}
+                    className="animate-load"
+                />
             </span>
             <span className="sr-only">Loading...</span>
         </>
