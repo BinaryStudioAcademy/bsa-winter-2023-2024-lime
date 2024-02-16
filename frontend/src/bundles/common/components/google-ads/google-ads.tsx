cce2cf78bce728b595a1ff765ec5730726f2eb24
velisssa <tybinka.vasylyna@gmail.com>
2024-02-16 11:02:10 +0100

    LIME-70: * run prettier

--- frontend/src/bundles/common/components/google-ads/google-ads.tsx
+++ frontend/src/bundles/common/components/google-ads/google-ads.tsx
@@ -4,8 +4,12 @@ type GoogleAdsProperties = {
 
 const GoogleAds: React.FC<GoogleAdsProperties> = ({ children }) => {
     return (
-        <div className='w-full bg-lm-yellow-200 flex flex-wrap items-center justify-center'>
-            {children || <p className='text-center text-lm-black-300'> Your advertisement may be here</p>}
+        <div className="bg-lm-yellow-200 flex w-full flex-wrap items-center justify-center">
+            {children || (
+                <p className="text-lm-black-300 text-center">
+                    Your advertisement may be here
+                </p>
+            )}
         </div>
     );
 };
