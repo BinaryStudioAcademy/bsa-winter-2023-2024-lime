ee2f5f4252e11b37a88d3a23dc15c5c9b78f756b
David <davidcharif@hotmail.com>
2024-02-16 15:46:29 -0500

    LIME-93: + Tailwind config for class dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'class',
     theme: {
         colors: {
             transparent: 'transparent',
