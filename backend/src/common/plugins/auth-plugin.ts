eb39395ad73840f49e3219fff2d70af041f61225
notlikethisss <anton3447@gmail.com>
2024-02-16 09:37:14 +0200

    LIME-56: * small fixes

--- backend/src/common/plugins/auth-plugin.ts
+++ backend/src/common/plugins/auth-plugin.ts
@@ -1,6 +1,7 @@
 import fastifyPlugin from 'fastify-plugin';
 import { UserValidationMessage } from 'shared';
 
+import { SERVED_PAGE_PATH } from '~/common/constants/constants.js';
 import { PluginName } from '~/common/enums/enums.js';
 import { extractTokenFromHeaders } from '~/common/helpers/helpers.js';
 import { HttpCode, HttpError } from '~/common/http/http.js';
@@ -12,6 +13,13 @@ const authPlugin = fastifyPlugin(
         fastify.decorateRequest('user', null);
 
         fastify.addHook('preHandler', async (request) => {
+
+            const isServedPagePath = request.routerPath === SERVED_PAGE_PATH;
+
+            if (isServedPagePath) {
+                return;
+            }
+
             if (whitelistedRoutes.includes(request.routeOptions.url)) {
                 return;
             }
