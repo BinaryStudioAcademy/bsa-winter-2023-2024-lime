d60ecbfe9c9a40e7249608eef64d5f4c187381d5
David <davidcharif@hotmail.com>
2024-02-16 15:57:41 -0500

    LIME-93: * Prettier fix

--- frontend/src/bundles/common/components/theme-switcher/theme-switcher.tsx
+++ frontend/src/bundles/common/components/theme-switcher/theme-switcher.tsx
@@ -1,24 +1,32 @@
-
 import { useCallback, useDarkTheme, useState } from '../../hooks/hooks.js';
 import { Switch } from './components/switch.js';
 
-function Switcher():JSX.Element {
-  const [colorTheme, setTheme] = useDarkTheme();
-  const [darkSide, setDarkSide] = useState<boolean>(colorTheme === 'dark' ? true : false);
+function Switcher(): JSX.Element {
+    const [colorTheme, setTheme] = useDarkTheme();
+    const [darkSide, setDarkSide] = useState<boolean>(
+        colorTheme === 'dark' ? true : false,
+    );
 
-  const toggleDarkMode = useCallback((checked: boolean) => {
-    const newTheme = checked ? 'dark' : 'light';
-    setTheme(newTheme);
-    setDarkSide(checked);
-  },[setTheme]);
+    const toggleDarkMode = useCallback(
+        (checked: boolean) => {
+            const newTheme = checked ? 'dark' : 'light';
+            setTheme(newTheme);
+            setDarkSide(checked);
+        },
+        [setTheme],
+    );
 
-  return (
-    <>
-      <div>
-        <Switch checked={darkSide} onChange={toggleDarkMode} size={56} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Switch
+                    checked={darkSide}
+                    onChange={toggleDarkMode}
+                    size={56}
+                />
+            </div>
+        </>
+    );
 }
 
 export { Switcher };
--- frontend/src/bundles/common/hooks/use-app-theme/use-app-theme.hook.ts
+++ frontend/src/bundles/common/hooks/use-app-theme/use-app-theme.hook.ts
@@ -4,7 +4,7 @@ type Theme = 'dark' | 'light';
 
 function useDarkTheme(): [Theme, React.Dispatch<React.SetStateAction<Theme>>] {
     const currentTheme = localStorage.getItem('theme');
-    const colorTheme = currentTheme === null ? 'dark' : currentTheme as Theme;
+    const colorTheme = currentTheme === null ? 'dark' : (currentTheme as Theme);
     const [theme, setTheme] = useState(colorTheme);
 
     useEffect(() => {
@@ -14,7 +14,6 @@ function useDarkTheme(): [Theme, React.Dispatch<React.SetStateAction<Theme>>] {
 
         // save theme to local storage
         localStorage.setItem('theme', theme);
-        
     }, [theme, colorTheme]);
 
     return [colorTheme, setTheme];
