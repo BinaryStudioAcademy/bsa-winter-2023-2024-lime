09adca61c82b44bcf2a19e910230fed10de9f10b
David <davidcharif@hotmail.com>
2024-02-16 09:40:30 -0500

    LIME-42: * Moved sidebar-nav inside sidebar

--- frontend/src/bundles/common/components/sidebar/components/sidebar-nav/sidebar-nav.tsx
+++ frontend/src/bundles/common/components/sidebar/components/sidebar-nav/sidebar-nav.tsx
@@ -1,26 +1,39 @@
+import { IconSize } from '~/bundles/common/components/icon/enums/enums.js';
+import { type IconName } from '~/bundles/common/components/icon/types/types.js';
 import { type AppRoute } from '~/bundles/common/enums/enums.js';
 import { useCallback, useNavigate } from '~/bundles/common/hooks/hooks.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
 
-import { Button, ButtonSize, ButtonVariant, Link } from '../components.js';
+import {
+    Button,
+    ButtonSize,
+    ButtonVariant,
+    Icon,
+    Link,
+} from '../../../components.js';
 
-interface SidebarNavProperties {
-    icon: JSX.Element;
+type SidebarNavProperties = {
     text: string;
+    icon: IconName;
     to: ValueOf<typeof AppRoute>;
     isActive?: boolean;
-}
+};
 
-const SidebarNav: React.FC<SidebarNavProperties> = ({
-    icon,
+const getIcon = (name: IconName): JSX.Element => {
+    return <Icon name={name} size={IconSize.LARGE} />;
+};
+
+const SidebarNav = ({
     text,
     to,
+    icon,
     isActive = false,
-}) => {
+}: SidebarNavProperties): JSX.Element => {
     const navigate = useNavigate();
     const handleNavigation = useCallback((): void => {
         navigate(to);
     }, [navigate, to]);
+    const iconElement = getIcon(icon);
     return (
         <Link
             to={to}
@@ -29,7 +42,7 @@ const SidebarNav: React.FC<SidebarNavProperties> = ({
             <Button
                 type="button"
                 label={text}
-                leftIcon={icon}
+                leftIcon={iconElement}
                 variant={ButtonVariant.SIDEBAR}
                 onClick={handleNavigation}
                 isActive={isActive}
