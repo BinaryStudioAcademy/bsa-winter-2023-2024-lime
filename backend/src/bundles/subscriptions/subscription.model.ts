378944e271f34247a62852c6ac0629d4c99c901f
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-18 20:08:00 +0200

    LIME-92: + subscription relation to plans and users

--- backend/src/bundles/subscriptions/subscription.model.ts
+++ backend/src/bundles/subscriptions/subscription.model.ts
@@ -1,7 +1,15 @@
+import { type RelationMappings, Model } from 'objection';
+
 import {
     AbstractModel,
     DatabaseTableName,
 } from '~/common/database/database.js';
+
+import { PlanAttributes } from '../plans/enums/enums.js';
+import { PlanModel } from '../plans/plan.model.js';
+import { UserAttributes } from '../users/enums/enums.js';
+import { UserModel } from '../users/user.model.js';
+import { SunscriptionAttributes } from './enums/enums.js';
 
 class SubscriptionModel extends AbstractModel {
     public 'userId': number;
@@ -10,6 +18,27 @@ class SubscriptionModel extends AbstractModel {
 
     public static override get tableName(): string {
         return DatabaseTableName.SUBSCRIPTIONS;
+    }
+
+    public static override get relationMappings(): RelationMappings {
+        return {
+            plans: {
+                relation: Model.HasOneRelation,
+                modelClass: PlanModel,
+                join: {
+                    from: `${DatabaseTableName.SUBSCRIPTIONS}.${SunscriptionAttributes.PLAN_ID}`,
+                    to: `${DatabaseTableName.PLANS}.${PlanAttributes.ID}`,
+                },
+            },
+            users: {
+                relation: Model.HasOneRelation,
+                modelClass: UserModel,
+                join: {
+                    from: `${DatabaseTableName.SUBSCRIPTIONS}.${SunscriptionAttributes.USER_ID}`,
+                    to: `${DatabaseTableName.PLANS}.${UserAttributes.ID}`,
+                },
+            },
+        };
     }
 }
 
