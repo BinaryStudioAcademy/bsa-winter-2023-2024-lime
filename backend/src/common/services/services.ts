cf07fa04ef89558155ca0152b0e3f2ee0819ae6c
jhladkov <zhenua.gladkovv@gmail.com>
2024-02-19 18:34:52 +0200

    LIME-68: * services.ts

--- backend/src/common/services/services.ts
+++ backend/src/common/services/services.ts
@@ -6,15 +6,17 @@ import { EmailService } from './email/email.service.js';
 import { FileService } from './file/file.service.js';
 
 const { API_KEY, FROM } = config.ENV.EMAIL;
+const { S3_REGION, S3_ACCESS_KEY, S3_SECRET_KEY, S3_BUCKET_NAME } =
+    config.ENV.AWS;
 
 const cryptService = new CryptService();
 const jwtService = new JwtService(config.ENV.APP.JWT_SECRET);
 const emailService = new EmailService(API_KEY, FROM);
 const fileService = new FileService({
-    accessKeyId: config.ENV.AWS.S3_ACCESS_KEY,
-    secretAccessKey: config.ENV.AWS.S3_SECRET_KEY,
-    region: config.ENV.AWS.S3_REGION,
-    bucketName: config.ENV.AWS.S3_BUCKET_NAME,
+    accessKeyId: S3_ACCESS_KEY,
+    secretAccessKey: S3_SECRET_KEY,
+    region: S3_REGION,
+    bucketName: S3_BUCKET_NAME,
 });
 
 export { cryptService, emailService, fileService, jwtService };
