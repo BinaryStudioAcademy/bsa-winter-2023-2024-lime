4d876f7da72f5195490f6f44a60153d9517a7707
David <davidcharif@hotmail.com>
2024-02-19 13:53:12 -0500

    LIME-93: + Add moon and sun icons for theme

--- frontend/src/bundles/common/components/icon/enums/icon-components.enum.ts
+++ frontend/src/bundles/common/components/icon/enums/icon-components.enum.ts
@@ -1,6 +1,8 @@
 import ArrowDownIcon from '~/assets/img/icons/arrow-down-icon.svg?react';
 import LogoIcon from '~/assets/img/icons/logo-icon.svg?react';
+import MoonIcon from '~/assets/img/icons/moon-icon.svg?react';
 import NotFoundIcon from '~/assets/img/icons/not-found-icon.svg?react';
+import SunIcon from '~/assets/img/icons/sun-icon.svg?react';
 
 import { type IconName } from '../types/icon.type.js';
 
@@ -11,6 +13,8 @@ const IconComponent: Record<
     arrowDown: ArrowDownIcon,
     logoIcon: LogoIcon,
     notFoundIcon: NotFoundIcon,
+    moonIcon: MoonIcon,
+    sunIcon: SunIcon
 } as const;
 
 export { IconComponent };
--- frontend/src/bundles/common/components/icon/types/icon.type.ts
+++ frontend/src/bundles/common/components/icon/types/icon.type.ts
@@ -1,3 +1,3 @@
-type IconName = 'arrowDown' | 'logoIcon' | 'notFoundIcon';
+type IconName = 'arrowDown' | 'logoIcon' | 'notFoundIcon' |'moonIcon' | 'sunIcon';
 
 export { type IconName };
