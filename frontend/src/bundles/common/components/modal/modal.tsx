81704d722b74e9cedbb59545a14c8b89ccbd4ebe
David <davidcharif@hotmail.com>
2024-02-19 14:41:46 -0500

    LIME-93: * Prepare clases for theme injection

--- frontend/src/bundles/common/components/modal/modal.tsx
+++ frontend/src/bundles/common/components/modal/modal.tsx
@@ -1,6 +1,8 @@
 import { XMarkIcon } from '@heroicons/react/16/solid';
 import { type ReactNode } from 'react';
 
+import { getValidClassNames } from '../../helpers/helpers.js';
+
 type Properties = {
     isOpen: boolean;
     title: string;
@@ -9,22 +11,30 @@ type Properties = {
 };
 
 const Modal: React.FC<Properties> = ({ isOpen, title, onClose, children }) => {
+    const classes = {
+        modalClass: `modal flex items-center justify-center ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'}`,
+        overlayClass:
+            'overlay bg-lm-black-400 fixed inset-0 z-50 cursor-pointer opacity-15',
+        contentClass:
+            'bg-lm-black-100 rounded-34 fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-start justify-center px-32 pb-24 pt-16 shadow-md transition-all',
+        closeIconClass:
+            'stroke-lm-grey-500 fill-lm-grey-500 hover:stroke-lm-yellow-100 hover:fill-lm-yellow-100 absolute right-8 top-8 h-3 w-3 cursor-pointer transition-all ',
+        titleClass: 'text-lm-grey-200 mb-10 text-xl font-extrabold leading-6 ',
+    };
     return (
-        <div
-            className={`modal flex items-center justify-center ${isOpen ? 'visible opacity-100' : 'invisible opacity-0'}`}
-        >
+        <div className={getValidClassNames(classes.modalClass)}>
             <div
-                className="overlay bg-lm-black-400 fixed inset-0 z-50 cursor-pointer opacity-15"
+                className={getValidClassNames(classes.overlayClass)}
                 onClick={onClose}
                 role="presentation"
             />
-            <div className="bg-lm-black-100 rounded-34 fixed fixed left-1/2 top-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-start justify-center px-32 pb-24 pt-16 shadow-md transition-all ">
+            <div className={getValidClassNames(classes.contentClass)}>
                 <XMarkIcon
                     onClick={onClose}
-                    className="stroke-lm-grey-500 fill-lm-grey-500 hover:stroke-lm-yellow-100 hover:fill-lm-yellow-100 absolute right-8 top-8 h-3 w-3 cursor-pointer transition-all "
+                    className={getValidClassNames(classes.closeIconClass)}
                 />
 
-                <h3 className="text-lm-grey-200 mb-10 text-xl font-extrabold leading-6">
+                <h3 className={getValidClassNames(classes.titleClass)}>
                     {title}
                 </h3>
 
