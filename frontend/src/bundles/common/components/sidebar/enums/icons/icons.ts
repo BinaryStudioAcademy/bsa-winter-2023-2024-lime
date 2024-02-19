660c76f76a6f2109f9e70aa962095ce3b2de25fe
David <davidcharif@hotmail.com>
2024-02-19 16:55:43 -0500

    LIME-42: * Extracted icons as a const on their folder

---
+++ frontend/src/bundles/common/components/sidebar/enums/icons/icons.ts
@@ -0,0 +1,10 @@
+const icons = {
+    OVERVIEW: 'overviewIcon',
+    GOALS: 'goalsIcon',
+    WORKOUT: 'workoutIcon',
+    HELP: 'helpIcon',
+    SCHEDULE: 'scheduleIcon',
+    LOGOUT: 'logoutIcon',
+} as const;
+
+export { icons };
