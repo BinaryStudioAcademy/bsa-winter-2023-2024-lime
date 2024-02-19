b22d0b3fb1636d85cf414dd7c646c6214838a33b
jhladkov <zhenua.gladkovv@gmail.com>
2024-02-19 18:29:38 +0200

    LIME-106: + add base openAI service implementation

---
+++ backend/src/common/services/open-ai/open-ai.service.ts
@@ -0,0 +1,9 @@
+class OpenAIService {
+    private apiKey: string;
+   public constructor(apiKey: string) {
+        this.apiKey = apiKey;
+    }
+
+}
+
+export { OpenAIService };
--- backend/src/common/services/services.ts
+++ backend/src/common/services/services.ts
@@ -3,11 +3,13 @@ import { JwtService } from '~/common/services/jwt/jwt.service.js';
 
 import { CryptService } from './crypt/crypt.service.js';
 import { EmailService } from './email/email.service.js';
+import { OpenAIService } from './open-ai/open-ai.service.js';
 
 const { API_KEY, FROM } = config.ENV.EMAIL;
 
 const cryptService = new CryptService();
 const jwtService = new JwtService(config.ENV.APP.JWT_SECRET);
 const emailService = new EmailService(API_KEY, FROM);
+const openAIService = new OpenAIService('');
 
-export { cryptService, emailService, jwtService };
+export { cryptService, emailService, jwtService, openAIService };
