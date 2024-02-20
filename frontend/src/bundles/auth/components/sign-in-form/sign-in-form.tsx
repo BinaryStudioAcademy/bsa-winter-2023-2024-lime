2141c452e51a58f354392ed7269e8700345458f1
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-20 17:09:52 +0200

    LIME-102: * fixed styles

--- frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
@@ -25,7 +25,7 @@ const SignInForm: React.FC<Properties> = ({ onSubmit, isLoading }) => {
     const { control, errors, handleSubmit } = useAppForm<UserAuthRequestDto>({
         defaultValues: DEFAULT_SIGN_IN_PAYLOAD,
         validationSchema: userAuthValidationSchema,
-        mode: 'onSubmit',
+        mode: 'onBlur',
     });
 
     const handleFormSubmit = useCallback(
