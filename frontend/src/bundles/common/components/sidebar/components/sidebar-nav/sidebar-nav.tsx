66221dc70a61eff5c0f95ae31b45c9682fc8a1e2
David <davidcharif@hotmail.com>
2024-02-20 08:19:15 -0500

    LIME-42: * Adjustments after merge

--- frontend/src/bundles/common/components/sidebar/components/sidebar-nav/sidebar-nav.tsx
+++ frontend/src/bundles/common/components/sidebar/components/sidebar-nav/sidebar-nav.tsx
@@ -1,16 +1,9 @@
-import { IconSize } from '~/bundles/common/components/icon/enums/enums.js';
 import { type IconName } from '~/bundles/common/components/icon/types/types.js';
 import { type AppRoute } from '~/bundles/common/enums/enums.js';
 import { useCallback, useNavigate } from '~/bundles/common/hooks/hooks.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
 
-import {
-    Button,
-    ButtonSize,
-    ButtonVariant,
-    Icon,
-    Link,
-} from '../../../components.js';
+import { Button, ButtonVariant, Icon, Link } from '../../../components.js';
 
 type SidebarNavProperties = {
     text: string;
@@ -20,7 +13,7 @@ type SidebarNavProperties = {
 };
 
 const getIcon = (name: IconName): JSX.Element => {
-    return <Icon name={name} size={IconSize.LARGE} />;
+    return <Icon name={name} size={'lg'} />;
 };
 
 const SidebarNav = ({
@@ -35,14 +28,14 @@ const SidebarNav = ({
     }, [navigate, to]);
     const iconElement = getIcon(icon);
     const classes = {
-        active: 'text-lm-black-100 hover:text-lm-black-200',
-        inactive: 'text-lm-grey-200 hover:text-lm-black-200',
+        active: 'text-lm-black-400 hover:text-lm-black-200',
+        inactive: 'text-lm-grey-200 hover:text-lm-black-400',
     };
 
     return (
         <Link
             to={to}
-            className={`${isActive ? classes.active : classes.inactive} flex items-center`}
+            className='flex items-center'
         >
             <Button
                 type="button"
@@ -52,7 +45,7 @@ const SidebarNav = ({
                 variant={ButtonVariant.SIDEBAR}
                 onClick={handleNavigation}
                 isActive={isActive}
-                size={ButtonSize.MEDIUM}
+                size={'md'}
             />
         </Link>
     );
