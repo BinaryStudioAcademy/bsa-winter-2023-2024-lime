1ca2966ec4e377ecbb18f3ee69f5ed62b3f679f8
David <davidcharif@hotmail.com>
2024-02-21 10:12:16 -0500

    LIME-124: * Prettier and added subtitle option

--- frontend/src/bundles/overview/components/goal-widget/goal-widget.tsx
+++ frontend/src/bundles/overview/components/goal-widget/goal-widget.tsx
@@ -4,30 +4,39 @@ import { CircularProgress } from './components/components.js';
 
 type WidgetProperties = {
     title: string;
+    subTitle?: string;
     value: number;
-    target: number;    
-} ;
+    target: number;
+};
 
 const GoalWidget = ({
     title = 'Track Your Daily Activities',
+    subTitle = '',
     value = 6,
-    target = 10
-}: WidgetProperties ) : JSX.Element => {
+    target = 10,
+}: WidgetProperties): JSX.Element => {
     return (
-        <div className="flex items-center max-w-[50rem] bg-gradient-to-r from-lm-yellow-100 to-transparent rounded-xl">
-            <div className="w-4/6 p-4 bg-gray-200">
-                <p className='font-bolder text-[24px]'>{title}</p>                
+        <div className="bg-goalWidget flex max-w-[50rem] items-center rounded-xl">
+            <div className="w-4/6 bg-gray-200 p-4">
+                <p className="font-bolder text-lm-black-100 text-[24px]">
+                    {title}
+                </p>
+                {subTitle && (
+                    <p className="text-lm-black-100 text-[14px]">{subTitle}</p>
+                )}
             </div>
-            <div className="w-3/6 flex items-center justify-end">
-                <div className="w-1/4 text-white flex">
-                    <Icon name='logoIcon' size='md'></Icon>
-                    <p className='font-extrabold text-md'>Exercises</p>
+            <div className="flex w-3/6 items-center justify-end">
+                <div className="flex w-1/4 text-white">
+                    <Icon name="logoIcon" size="md"></Icon>
+                    <p className="text-md font-extrabold">Exercises</p>
                 </div>
-                <div className="w-2/4 flex items-center justify-center">
+                <div className="flex w-2/4 items-center justify-center">
                     <CircularProgress value={value} target={target} />
-                    <div className='absolute text-white'>
-                        <p className='text-4xl inline-flex font-extrabold font-gilroy'>{value}</p>
-                        <p className='inline-flex font-normal'>/{target}</p>
+                    <div className="absolute text-white">
+                        <p className="font-gilroy inline-flex text-4xl font-extrabold">
+                            {value}
+                        </p>
+                        <p className="inline-flex font-normal">/{target}</p>
                     </div>
                 </div>
             </div>
