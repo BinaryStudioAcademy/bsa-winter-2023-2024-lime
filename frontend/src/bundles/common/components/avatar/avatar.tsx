480fd3386e2e3b3d23d0c77240d9faea5029bdd7
David <davidcharif@hotmail.com>
2024-02-21 09:01:57 -0500

    LIME-134: * Suggested change

--- frontend/src/bundles/common/components/avatar/avatar.tsx
+++ frontend/src/bundles/common/components/avatar/avatar.tsx
@@ -9,7 +9,7 @@ type AvatarSize = Exclude<
 type Properties = {
     size: AvatarSize;
     email: string;
-    avatarUrl: string;
+    avatarUrl: string | null;
 };
 
 const sizeToClass: Record<AvatarSize, string> = {
