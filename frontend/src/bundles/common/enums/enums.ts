2d82d8e8463061455a57b9449cb5b01dade269a4
David <davidcharif@hotmail.com>
2024-02-21 09:47:00 -0500

    LIME-93: - ThemeCompose deleted from files

--- frontend/src/bundles/common/enums/enums.ts
+++ frontend/src/bundles/common/enums/enums.ts
@@ -2,5 +2,4 @@ export { AppRoute } from './app-route.enum.js';
 export { ComponentSize } from './component-size.enum.js';
 export { DataStatus } from './data-status.enum.js';
 export { EventKey } from './event-keys.js';
-export { ThemeCompose } from './theme-colors.js';
 export { ApiPath, AppEnvironment, ContentType, ServerErrorType } from 'shared';
