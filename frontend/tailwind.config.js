2673f43f9754da99843d9da7c022133f50a73350
David <davidcharif@hotmail.com>
2024-02-21 10:10:55 -0500

    LIME-124: * Gradient goal widget added to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -71,6 +71,7 @@ export default {
                 'auth': 'linear-gradient(to right, #E0FE10 20%, transparent 130%), linear-gradient(to right, #2A2F37 20%, transparent 100%), url("./src/assets/img/auth-bg.png")',
                 'rectangle':
                     'linear-gradient(89.83deg, #E0FE10 36.42%, rgba(224, 254, 16, 0) 125.23%)',
+                'goalWidget': 'linear-gradient(to right, #E0FE10 20%, transparent 130%), linear-gradient(to right, #2A2F37 20%, transparent 100%)'
             },
             borderRadius: {
                 '34': '2.1rem',
