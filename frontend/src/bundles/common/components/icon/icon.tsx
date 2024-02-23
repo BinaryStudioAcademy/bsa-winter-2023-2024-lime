1e340eb20b94e4c902881a30be78c590edbdf33f
David <davidcharif@hotmail.com>
2024-02-23 13:06:24 -0500

    LIME-42: * Extracted sizeToClass for reusability

--- frontend/src/bundles/common/components/icon/icon.tsx
+++ frontend/src/bundles/common/components/icon/icon.tsx
@@ -1,23 +1,17 @@
 import { type ValueOf } from 'shared/src/types/types.js';
 
-import { ComponentSize } from '~/bundles/common/enums/component-size.enum.js';
+import { type ComponentSize } from '~/bundles/common/enums/component-size.enum.js';
 import { getValidClassNames } from '~/bundles/common/helpers/helpers.js';
 
 import { type IconColor, type IconName } from './enums/enums.js';
 import { IconComponent } from './enums/icon-components.enum.js';
+import { sizeToClass } from './helpers/helpers.js';
 
 type Properties = {
     name: ValueOf<typeof IconName>;
     color?: ValueOf<typeof IconColor>;
     size?: ValueOf<typeof ComponentSize>;
     className?: string;
-};
-
-const sizeToClass: Record<ValueOf<typeof ComponentSize>, string> = {
-    [ComponentSize.SMALL]: 'h-5 w-5',
-    [ComponentSize.MEDIUM]: 'h-6 w-6',
-    [ComponentSize.LARGE]: 'h-8 w-8',
-    [ComponentSize.EXTRA_LARGE]: 'h-40 w-40',
 };
 
 const Icon = ({ name, color, size, className }: Properties): JSX.Element => {
@@ -34,4 +28,4 @@ const Icon = ({ name, color, size, className }: Properties): JSX.Element => {
     );
 };
 
-export { Icon, sizeToClass };
+export { Icon };
