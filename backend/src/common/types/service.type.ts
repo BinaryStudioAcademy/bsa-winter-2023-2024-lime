fd0d8b83f782c8ce800d0d8f824fc028665f9925
valery-chumak <blossssom88@gmail.com>
2024-02-26 12:49:23 +0200

    LIME-69: * edit update service type

--- backend/src/common/types/service.type.ts
+++ backend/src/common/types/service.type.ts
@@ -1,15 +1,10 @@
-import { type UserUpdateProfileRequestDto } from 'shared';
-
 type Service<T = unknown> = {
     find(query: Record<string, T>): Promise<T>;
     findAll(): Promise<{
         items: T[];
     }>;
     create(payload: unknown): Promise<T>;
-    update(
-        id: number,
-        updatedDetails: UserUpdateProfileRequestDto,
-    ): Promise<T | null>;
+    update(id: number, updatedDetails: unknown): Promise<T | null>;
     delete(): Promise<boolean>;
 };
 
