3dc90472f4858d473fd0621ae5db0de09a0ebb17
David <davidcharif@hotmail.com>
2024-02-26 01:04:15 -0500

    LIME-139: * Styles for notifications

--- frontend/src/bundles/common/components/header/components/notifications/notifications-header.tsx
+++ frontend/src/bundles/common/components/header/components/notifications/notifications-header.tsx
@@ -8,13 +8,18 @@ type Properties = {
 const NotificationIcon = ({ count, onClick }: Properties): JSX.Element => {
     return (
         <button
-            className="relative flex items-center justify-center"
+            className="group relative flex items-center justify-center"
             onClick={onClick}
         >
-            <BellIcon className="text-lm-grey-200 w-8 sm:w-6" />
-            <span className="bg-lm-yellow-100 absolute -right-1 -top-2 flex h-full max-h-4 w-full max-w-4 items-center justify-center rounded-full text-xs font-extrabold">
-                {count}
-            </span>
+            <BellIcon
+                className="text-lm-grey-200 group-hover:text-lm-grey-500 w-8 transform transition-all duration-300 ease-in-out group-hover:rotate-12 group-hover:scale-110 sm:w-6
+            "
+            />
+            {count > 0 && (
+                <span className="bg-lm-yellow-100 group-hover:bg-lm-yellow-200 absolute -right-1 -top-2 flex h-full max-h-4 w-full max-w-4 items-center justify-center rounded-full text-xs font-extrabold">
+                    {count}
+                </span>
+            )}
         </button>
     );
 };
