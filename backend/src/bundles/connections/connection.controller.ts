bb8510332ba4782f771b4550ba3019cb82b54ae2
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-02-27 18:16:31 +0200

    LIME-81: + connections swagger

--- backend/src/bundles/connections/connection.controller.ts
+++ backend/src/bundles/connections/connection.controller.ts
@@ -11,6 +11,58 @@ import { type Logger } from '~/common/logger/logger.js';
 
 import { ConnectionsPath } from './enums/enums.js';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Error:
+ *       type: object
+ *       properties:
+ *         errorType:
+ *           type: string
+ *           enum:
+ *              - COMMON
+ *              - VALIDATION
+ *         message:
+ *           type: string
+ *
+ */
+
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      OAuth:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: number
+ *            format: number
+ *            minimum: 1
+ *          userId:
+ *            type: number
+ *            format: number
+ *            minimum: 1
+ *          tokenType:
+ *            type: string
+ *          expiresAt:
+ *            type: number
+ *            format: number
+ *            minimum: 0
+ *          accessToken:
+ *            type: string
+ *          refreshToken:
+ *            type: string
+ *          scope:
+ *            type: string
+ *            nullable: true
+ *          provider:
+ *            type: string
+ *            enum:
+ *              - strava
+ *              - google-fit
+ */
+
 class ConnectionController extends BaseController {
     private oAuthService: OAuthService;
 
@@ -31,6 +83,36 @@ class ConnectionController extends BaseController {
                 ),
         });
     }
+
+    /**
+     * @swagger
+     * /api/v1/connections/:
+     *    get:
+     *      tags:
+     *       - Connections
+     *      description: Returns an array of current user connections
+     *      security:
+     *        - bearerAuth: []
+     *      responses:
+     *        200:
+     *          description: Successful operation
+     *          content:
+     *            application/json:
+     *              schema:
+     *                 type: object
+     *                 properties:
+     *                   items:
+     *                     type: array
+     *                     items:
+     *                       $ref: '#/components/schemas/OAuth/'
+     *        400:
+     *          description: Failed operation
+     *          content:
+     *              application/json:
+     *                  schema:
+     *                      type: object
+     *                      $ref: '#/components/schemas/Error'
+     */
 
     private async getUserConnections(
         options: ApiHandlerOptions<{
