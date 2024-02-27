dbee68eeae71c66cb487498eb200f065513bcbed
jhladkov <zhenua.gladkovv@gmail.com>
2024-02-27 18:00:16 +0200

    LIME-142: + migration

---
+++ backend/src/migrations/20240227113506_add_workouts_table.ts
@@ -0,0 +1,69 @@
+import { type Knex } from 'knex';
+
+const TABLE_NAME = 'workouts';
+const USERS_TABLE_NAME = 'users';
+
+const ColumnName = {
+    ID: 'id',
+    USER_ID: 'user_id',
+    ACTIVITY: 'activity',
+    STEPS: 'steps',
+    HEART_RATE: 'heart_rate',
+    START_TIME: 'start_time',
+    END_TIME: 'end_time',
+    DISTANCE: 'distance',
+    SPEED: 'speed',
+    KILOCALORIES: 'kilocalories',
+    CREATED_AT: 'created_at',
+    UPDATED_AT: 'updated_at',
+};
+
+const ACTIVITY_ENUM = `${ColumnName.ACTIVITY}_enum`;
+
+const Activity = {
+    CYCLING: 'cycling',
+    RUNNING: 'running',
+    WALKING: 'walking',
+} as const;
+async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable(TABLE_NAME, (table) => {
+        table.increments(ColumnName.ID).primary();
+        table
+            .integer(ColumnName.USER_ID)
+            .unsigned()
+            .notNullable()
+            .references(ColumnName.ID)
+            .inTable(USERS_TABLE_NAME)
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE');
+        table.integer(ColumnName.STEPS).nullable();
+        table.integer(ColumnName.KILOCALORIES).notNullable();
+        table.integer(ColumnName.HEART_RATE).notNullable();
+        table
+            .dateTime(ColumnName.START_TIME)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table.dateTime(ColumnName.END_TIME).nullable();
+        table.integer(ColumnName.DISTANCE).notNullable();
+        table.integer(ColumnName.SPEED).notNullable();
+        table.enum(ColumnName.ACTIVITY, Object.values(Activity), {
+            useNative: true,
+            enumName: `${ColumnName.ACTIVITY}_enum`,
+        });
+        table
+            .dateTime(ColumnName.CREATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+        table
+            .dateTime(ColumnName.UPDATED_AT)
+            .notNullable()
+            .defaultTo(knex.fn.now());
+    });
+}
+
+async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTableIfExists(TABLE_NAME);
+    await knex.schema.raw(`DROP TYPE IF EXISTS ${ACTIVITY_ENUM}`);
+}
+
+export { down, up };
