cf3d65fbb8d8b8e55f58d8a59c5c4c5ed4a43d34
stpalamar <stanislav6121@gmail.com>
2024-02-27 18:35:29 +0200

    LIME-192: * fix pw reset success message aligning

--- frontend/src/bundles/password-reset/components/password-reset-success-message/password-reset-success-message.tsx
+++ frontend/src/bundles/password-reset/components/password-reset-success-message/password-reset-success-message.tsx
@@ -4,15 +4,18 @@ import { AppRoute } from '~/bundles/common/enums/app-route.enum.js';
 
 const PasswordResetSuccessMessage: React.FC = () => {
     return (
-        <div className="flex flex-col items-center gap-16">
-            <SuccessIcon className="fill-lm-yellow-100 w-32" />
-            <p className="text-primary text-xl leading-7">
-                Your password has been successfully reset. You can now use it to{' '}
-                <Link to={AppRoute.SIGN_IN}>
-                    <span className="text-action">log in</span>
-                </Link>
-            </p>
-        </div>
+        <>
+            <SuccessIcon className="fill-lm-yellow-100 mx-auto w-32" />
+            <div className="flex flex-col items-center gap-16">
+                <p className="text-primary text-xl leading-7">
+                    Your password has been successfully reset. You can now use
+                    it to{' '}
+                    <Link to={AppRoute.SIGN_IN}>
+                        <span className="text-action">log in</span>
+                    </Link>
+                </p>
+            </div>
+        </>
     );
 };
 
