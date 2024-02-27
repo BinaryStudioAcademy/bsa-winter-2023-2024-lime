c8f9812039e9cd82aa1e214f8ed2e866a48b851d
Artem Lymarenko <artemchiklym@gmail.com>
2024-02-27 12:32:05 +0200

    LIME-92: + added documentation for subscription plan controller

--- backend/src/bundles/subscription-plans/subscription-plan.controller.ts
+++ backend/src/bundles/subscription-plans/subscription-plan.controller.ts
@@ -7,6 +7,46 @@ import { type Logger } from '~/common/logger/types/types.js';
 import { SubscriptionPlansApiPath } from './enums/enums.js';
 import { type SubscriptionPlanService } from './subscription-plan.service.js';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Error:
+ *       type: object
+ *       properties:
+ *         errorType:
+ *           type: string
+ *           enum:
+ *              - COMMON
+ *              - VALIDATION
+ *         message:
+ *           type: string
+ *
+ */
+
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      SubscriptionPlan:
+ *        type: object
+ *        properties:
+ *         id:
+ *           type: integer
+ *           minimum: 1
+ *         name:
+ *           type: string
+ *         price:
+ *           type: number
+ *           format: double
+ *         description:
+ *           type: string
+ *           nullable: true
+ *         stripeProductId:
+ *           type: string
+ *         stripePriceId:
+ *           type: string
+ */
 class SubscriptionPlanController extends BaseController {
     private subscriptionPlanService: SubscriptionPlanService;
 
@@ -25,6 +65,33 @@ class SubscriptionPlanController extends BaseController {
         });
     }
 
+    /**
+     * @swagger
+     * /api/v1/subscription-plans/:
+     *    get:
+     *      tags:
+     *       - SubscriptionPlans
+     *      description: Returns an array of subscription plans
+     *      responses:
+     *        200:
+     *          description: Successful operation
+     *          content:
+     *            application/json:
+     *              schema:
+     *                 type: object
+     *                 properties:
+     *                   items:
+     *                     type: array
+     *                     items:
+     *                       $ref: '#/components/schemas/SubscriptionPlan'
+     *        401:
+     *          description: Failed operation
+     *          content:
+     *              application/json:
+     *                  schema:
+     *                      type: object
+     *                      $ref: '#/components/schemas/Error'
+     */
     private async findAll(): Promise<ApiHandlerResponse> {
         return {
             status: HttpCode.OK,
