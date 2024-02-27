690cdc7d8e1631647ec6ddfb6358327f60707bbe
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-02-27 23:09:57 +0200

    LIME-81: - snakeToCamel helper

--- backend/src/common/helpers/helpers.ts
+++ backend/src/common/helpers/helpers.ts
@@ -1,3 +1,2 @@
 export { createProtectedRoutes } from './create-protected-routes-helper.js';
 export { extractTokenFromHeaders } from './extract-token-helper.js';
-export { snakeToCamel } from './snake-to-camel-helper.js';
