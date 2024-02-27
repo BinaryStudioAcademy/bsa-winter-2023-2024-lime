da3dce73831c467b34b76480f45b95bd736f5ed9
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-02-27 19:17:24 +0200

    LIME-81: * fix prettier issue

--- backend/src/bundles/oauth/enums/enums.ts
+++ backend/src/bundles/oauth/enums/enums.ts
@@ -2,4 +2,10 @@ export { ErrorMessage } from './error-message.enum.js';
 export { OAuthInfoAttributes } from './oauth-info-attributes.enum.js';
 export { OAuthProvider } from './oauth-provider.enum.js';
 export { OAuthStateAttributes } from './oauth-state-attributes.js';
-export { ApiPath, ConnectionsPath, HttpCode, HttpError, OAuthActionsPath } from 'shared';
+export {
+    ApiPath,
+    ConnectionsPath,
+    HttpCode,
+    HttpError,
+    OAuthActionsPath,
+} from 'shared';
