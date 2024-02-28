e2653edf958aaae5753920d70b9edf79542323bc
David <davidcharif@hotmail.com>
2024-02-28 10:48:30 -0500

    LIME-199: * Requested changes

--- frontend/src/bundles/overview/components/chart-goal-progress/chart-goal-progress.tsx
+++ frontend/src/bundles/overview/components/chart-goal-progress/chart-goal-progress.tsx
@@ -37,7 +37,7 @@ const colors = {
     },
 };
 
-type Data = {
+type BarChartData = {
     labels: string[];
     datasets: {
         label: string;
@@ -47,114 +47,114 @@ type Data = {
     }[];
 };
 
+const weeklyData = {
+    labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ],
+    datasets: [
+        {
+            label: 'Workout',
+            data: [20, 30, 40, 50, 60, 70, 80],
+            backgroundColor: colors.workout.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+        {
+            label: 'Calories',
+            data: [50, 60, 70, 80, 90, 100, 11],
+            backgroundColor: colors.calories.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+        {
+            label: 'Steps',
+            data: [100, 20, 30, 40, 50, 60, 70],
+            backgroundColor: colors.steps.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+    ],
+};
+
+const monthlyData = {
+    labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+    datasets: [
+        {
+            label: 'Workout',
+            data: [20, 30, 40, 50],
+            backgroundColor: colors.workout.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+        {
+            label: 'Calories',
+            data: [50, 60, 70, 80],
+            backgroundColor: colors.calories.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+        {
+            label: 'Steps',
+            data: [100, 20, 30, 40],
+            backgroundColor: colors.steps.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+    ],
+};
+
+const yearlyData = {
+    labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ],
+    datasets: [
+        {
+            label: 'Workout',
+            data: [20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 50, 55],
+            backgroundColor: colors.workout.backgroundColor,
+            borderColor: 'transparent',
+            margin: 10,
+        },
+        {
+            label: 'Calories',
+            data: [50, 60, 70, 80, 90, 100, 100, 20, 30, 40, 50, 16],
+            backgroundColor: colors.calories.backgroundColor,
+
+            borderColor: 'transparent',
+        },
+        {
+            label: 'Steps',
+            data: [10, 20, 30, 40, 0, 60, 70, 8, 90, 100, 11, 12],
+            backgroundColor: colors.steps.backgroundColor,
+            borderColor: 'transparent',
+        },
+    ],
+};
+
 const ChartGoalProgress = (): JSX.Element => {
     const [currentData, setCurrentData] = useState<SelectOption>({
         value: 'weekly',
         label: 'Weekly',
     });
 
-    const weeklyData = {
-        labels: [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-        ],
-        datasets: [
-            {
-                label: 'Workout',
-                data: [20, 30, 40, 50, 60, 70, 80],
-                backgroundColor: colors.workout.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-            {
-                label: 'Calories',
-                data: [50, 60, 70, 80, 90, 100, 11],
-                backgroundColor: colors.calories.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-            {
-                label: 'Steps',
-                data: [100, 20, 30, 40, 50, 60, 70],
-                backgroundColor: colors.steps.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-        ],
-    };
-
-    const monthlyData = {
-        labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-        datasets: [
-            {
-                label: 'Workout',
-                data: [20, 30, 40, 50],
-                backgroundColor: colors.workout.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-            {
-                label: 'Calories',
-                data: [50, 60, 70, 80],
-                backgroundColor: colors.calories.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-            {
-                label: 'Steps',
-                data: [100, 20, 30, 40],
-                backgroundColor: colors.steps.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-        ],
-    };
-
-    const yearlyData = {
-        labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December',
-        ],
-        datasets: [
-            {
-                label: 'Workout',
-                data: [20, 30, 40, 50, 60, 70, 80, 90, 100, 70, 50, 55],
-                backgroundColor: colors.workout.backgroundColor,
-                borderColor: 'transparent',
-                margin: 10,
-            },
-            {
-                label: 'Calories',
-                data: [50, 60, 70, 80, 90, 100, 100, 20, 30, 40, 50, 16],
-                backgroundColor: colors.calories.backgroundColor,
-
-                borderColor: 'transparent',
-            },
-            {
-                label: 'Steps',
-                data: [10, 20, 30, 40, 0, 60, 70, 8, 90, 100, 11, 12],
-                backgroundColor: colors.steps.backgroundColor,
-                borderColor: 'transparent',
-            },
-        ],
-    };
-
-    const options = {
+    const optionsBarChart = {
         scales: {
             y: {
                 beginAtZero: true,
@@ -188,7 +188,7 @@ const ChartGoalProgress = (): JSX.Element => {
         },
     };
 
-    const dataMapping: Record<string, Data> = {
+    const dataMapping: Record<string, BarChartData> = {
         'weekly': weeklyData,
         'monthly': monthlyData,
         'yearly': yearlyData,
@@ -202,7 +202,7 @@ const ChartGoalProgress = (): JSX.Element => {
     });
 
     const handleChange = useCallback((newValue: SingleValue<SelectOption>) => {
-        if (newValue !== null) {
+        if (newValue) {
             setCurrentData(newValue);
         }
     }, []);
@@ -224,8 +224,8 @@ const ChartGoalProgress = (): JSX.Element => {
                 </div>
             </div>
             <BarChart
-                chartData={dataMapping[currentData.value] || weeklyData}
-                charOptions={options}
+                chartData={dataMapping[currentData.value] ?? weeklyData}
+                charOptions={optionsBarChart}
             />
         </div>
     );
