959c5536bfa86122190d21defe7a807d2346275f
Sergii Ternytskyi <serhij86@gmail.com>
2024-02-29 20:55:03 +0200

    LIME-229: * fixed password forgot succsess messache showing

--- frontend/src/bundles/auth/components/password-forgot-succsess-message/password-forgot-succsess-message.tsx
+++ frontend/src/bundles/auth/components/password-forgot-succsess-message/password-forgot-succsess-message.tsx
@@ -4,7 +4,7 @@ const PasswordForgotSuccessMessage: React.FC = () => {
     return (
         <div className="flex flex-col items-center gap-16">
             <SuccessIcon className="fill-lm-yellow-100 w-32" />
-            <p className="text-primary text-xl leading-7">
+            <p className="text-primary text-base leading-7 md:text-xl">
                 Link for password reset was sent to your email. Check your inbox
                 letters and follow the instructions. If you don’t see the email,
                 please check your spam folder
