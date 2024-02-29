a1675069d0ec2360eebe5532732e0b8caa14568c
jhladkov <zhenua.gladkovv@gmail.com>
2024-02-29 18:58:11 +0200

    LIME-197: * minor changes

--- backend/src/bundles/google-fit/constants/constants.ts
+++ backend/src/bundles/google-fit/constants/constants.ts
@@ -1,3 +1,6 @@
-export { GOOGLE_FIT_ACCESS_TYPE } from './google-fit-access-type.js';
-export { GOOGLE_FIT_API_URL } from './google-fit-api-url.js';
-export { READ_SCOPE, WRITE_SCOPE } from './google-fit-required-scopes.js';
+export { GOOGLE_FIT_ACCESS_TYPE } from './google-fit-access-type.constant.js';
+export { GOOGLE_FIT_API_URL } from './google-fit-api-url.constant.js';
+export {
+    READ_SCOPE,
+    WRITE_SCOPE,
+} from './google-fit-required-scopes.constant.js';
