d48a09adde8cb0c8c9647420ab9595e5b584bcb6
David <davidcharif@hotmail.com>
2024-02-29 17:27:35 -0500

    LIME-199: * Prettier

--- frontend/src/bundles/overview/components/charts/types/types.ts
+++ frontend/src/bundles/overview/components/charts/types/types.ts
@@ -1,6 +1,2 @@
-export {
-    type BarChartData,
-} from './bar-chart-data.types.js';
-export {
-    type BarChartOptions,
-} from './bar-chart-options.types.js';
+export { type BarChartData } from './bar-chart-data.types.js';
+export { type BarChartOptions } from './bar-chart-options.types.js';
