4b18550132f6e5ab536b8bc0caa7f30e6fbc576b
Sergii Ternytskyi <serhij86@gmail.com>
2024-03-04 12:27:06 +0200

    LIME-268: * fix same password message

--- shared/src/bundles/password-reset/enums/password-reset-validation-message.enum.ts
+++ shared/src/bundles/password-reset/enums/password-reset-validation-message.enum.ts
@@ -1,7 +1,7 @@
 const PasswordResetValidationMessage = {
     USER_NOT_FOUND: 'User not found',
     TOKEN_EXPIRED: 'The password reset token you used has expired',
-    SAME_PASSWORD: 'The password should differ from the previous',
+    SAME_PASSWORD: 'The password should differ from the previous one',
     SERVER_ERROR: 'Something went wrong',
     EMAIL_NOT_SENT: 'Something went wrong. Email has not sent',
     PASSWORDS_NOT_EQUAL: 'Passwords are not equal',
