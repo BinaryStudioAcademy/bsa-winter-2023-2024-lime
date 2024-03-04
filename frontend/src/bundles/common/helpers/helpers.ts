342345872e6bcc41149a06321c670d067318e5da
David <davidcharif@hotmail.com>
2024-03-04 16:23:10 -0500

    LIME-201: * Correct helper name

--- frontend/src/bundles/common/helpers/helpers.ts
+++ frontend/src/bundles/common/helpers/helpers.ts
@@ -1,5 +1,5 @@
 export { applyThemeClassname } from './apply-theme-classname/apply-theme-classname.helper.js';
 export { getUserId } from './get-user-id/get-user-id.js';
 export { getValidClassNames } from './get-valid-class-names/get-valid-class-names.helper.js';
-export { validateImageUrl } from './validate-image-url/validate-image-url.js';
+export { validateImageUrl } from './validate-image-url/validate-image-url.helper.js';
 export { configureString } from 'shared';
