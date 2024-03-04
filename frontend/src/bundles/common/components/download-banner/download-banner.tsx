dad6b6fded8bd8747084b2119ec908b0db48eb18
David <davidcharif@hotmail.com>
2024-03-04 10:55:32 -0500

    LIME-219: * Background ligth download banner

--- frontend/src/bundles/common/components/download-banner/download-banner.tsx
+++ frontend/src/bundles/common/components/download-banner/download-banner.tsx
@@ -76,7 +76,7 @@ const DownloadBanner = (): JSX.Element | null => {
         return null;
     }
     return (
-        <div className="bg-lm-yellow-200 text-primary fixed left-0 top-0 z-50 flex h-24 w-full items-center justify-evenly rounded-b-lg p-4 text-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] lg:xl:hidden">
+        <div className="bg-buttonSecondary text-primary fixed left-0 top-0 z-50 flex h-24 w-full items-center justify-evenly rounded-b-lg p-4 text-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] lg:xl:hidden">
             <button
                 onClick={closeBanner}
                 className="text-lm-black-300 hover:text-primary ml-1 mr-3 h-12 w-12"
