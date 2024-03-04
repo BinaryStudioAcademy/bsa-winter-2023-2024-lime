09edfd6e8c140cc423190775ab958ce5249a32ca
valery-chumak <blossssom88@gmail.com>
2024-03-04 23:11:21 +0200

    LIME-69: * changed validation messages

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -1,6 +1,6 @@
 import { UserValidationRule } from './user-validation-rule.enum.js';
 
-const { PASSWORD } = UserValidationRule;
+const { PASSWORD, FULLNAME, NICKNAME, WEIGHT, HEIGHT } = UserValidationRule;
 
 const UserValidationMessage = {
     REQUIRED: 'Please fill out this field',
@@ -12,17 +12,17 @@ const UserValidationMessage = {
     LOGIN_CREDENTIALS_DO_NOT_MATCH: 'Incorrect email or password',
     TOKEN_REQUIRE: 'Token is required',
     TOKEN_INVALID: 'Invalid token',
-    FULLNAME_LENGTH: 'Fullname should have from 2 to 60 characters.',
+    FULLNAME_LENGTH: `Fullname should have from ${FULLNAME.MIN_LENGTH}  to ${FULLNAME.MAX_LENGTH} characters.`,
     FULLNAME_WRONG:
         'Fullname should consist of letters, special characters, spaces, emoji',
-    NICKNAME_LENGTH: 'Nickname should have from 2 to 30 characters.',
+    NICKNAME_LENGTH: `Nickname should have from ${NICKNAME.MIN_LENGTH} to ${NICKNAME.MAX_LENGTH} characters.`,
     NICKNAME_WRONG:
         'Nickname should consist of letters, special characters, spaces, emoji',
     BIRTHDATE_FORMAT: 'Invalid date format. Use DD/MM/YYYY',
-    WEIGHT_VALUE: 'Weight should be from 1 to 300 kg.',
+    WEIGHT_VALUE: `Weight should be from ${WEIGHT.MIN_VALUE} to ${WEIGHT.MAX_VALUE} kg.`,
     WEIGHT_WRONG: 'Weight should consist of digits.',
     HEIGHT_WRONG: 'Height should consist of digits.',
-    HEIGHT_VALUE: 'Height should be from 1 to 300 sm.',
+    HEIGHT_VALUE: `Height should be from ${HEIGHT.MIN_VALUE} to ${HEIGHT.MIN_VALUE} sm.`,
     USER_NOT_FOUND: 'User not found',
 } as const;
 
