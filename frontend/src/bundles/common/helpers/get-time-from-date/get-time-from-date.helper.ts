8c1134c72f4a3c4168f1c67a62d59b0f4c1a00b9
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-03-05 23:54:22 +0200

    LIME-143: * getTimeFromDate helper

--- frontend/src/bundles/common/helpers/get-time-from-date/get-time-from-date.helper.ts
+++ frontend/src/bundles/common/helpers/get-time-from-date/get-time-from-date.helper.ts
@@ -1,13 +1,9 @@
+import { format } from 'date-fns';
+
 const getTimeFromDate = (date: Date | null): string => {
     const dateTime = date ? new Date(date) : new Date();
 
-    const hours = dateTime.getHours();
-    const minutes = dateTime.getMinutes();
-
-    const hoursString = hours >= 10 ? String(hours) : `0${hours}`;
-    const minutesString = minutes >= 10 ? String(minutes) : `0${minutes}`;
-
-    return `${hoursString}:${minutesString}`;
+    return format(dateTime, 'HH:mm');
 };
 
 export { getTimeFromDate };
