22199cf39afc2f4fa10b947b84324b41ca8f005d
jhladkov <zhenua.gladkovv@gmail.com>
2024-03-06 17:04:04 +0200

    LIME-196: * minor changes

--- backend/src/bundles/strava/constants/strava-api-url.constant.ts
+++ backend/src/bundles/strava/constants/strava-api-url.constant.ts
@@ -1,7 +1,7 @@
 import { STRAVA_URL } from './strava-url.constant.js';
 
 const STRAVA_API_URL = `${STRAVA_URL}/api/v3`;
-const STRAVA_ACTIVITIES_URL = `${STRAVA_URL}/api/v3/activities`;
+const STRAVA_ACTIVITIES_URL = `${STRAVA_API_URL}/activities`;
 const STRAVA_SUBSCRIPTION_URL = `${STRAVA_API_URL}/push_subscriptions`;
 
 export { STRAVA_ACTIVITIES_URL, STRAVA_API_URL, STRAVA_SUBSCRIPTION_URL };
