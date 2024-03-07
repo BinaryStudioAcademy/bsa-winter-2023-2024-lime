92f01075348fa682b6f33ab8b25941fb1cf3524f
David <davidcharif@hotmail.com>
2024-03-07 11:00:43 -0500

    LIME-139: * Prettier

--- frontend/src/bundles/notifications/types/notification-state-type-slice.ts
+++ frontend/src/bundles/notifications/types/notification-state-type-slice.ts
@@ -1,13 +1,13 @@
-import { type  ValueOf } from 'shared';
+import { type ValueOf } from 'shared';
 
-import { type  DataStatus } from '~/bundles/common/enums/enums.js';
+import { type DataStatus } from '~/bundles/common/enums/enums.js';
 
 import { type NotificationResponseDto } from '../temp-types/types.js';
 
-type NotificationStateTypeSlice ={
+type NotificationStateTypeSlice = {
     notifications: Array<NotificationResponseDto>;
     dataStatus: ValueOf<typeof DataStatus>;
     isRefreshing: boolean;
-    };
+};
 
 export { type NotificationStateTypeSlice };
