b0dfc8395ac3438005c5617ccaf6e484a0b195cb
JKaypa <jose_kaypa@hotmail.com>
2024-03-08 11:22:47 -0500

    LIME-153: * move file type to .d.ts file

--- backend/fastify-types.d.ts
+++ backend/fastify-types.d.ts
@@ -1,9 +1,11 @@
 import { type FastifyRequest as OriginalFastifyRequest } from 'fastify';
+import { type File } from 'fastify-multer/lib/interfaces.js';
 
 import { type UserAuthResponseDto } from '~/bundles/users/users.js';
 
 declare module 'fastify' {
     interface FastifyRequest extends OriginalFastifyRequest {
         user: UserAuthResponseDto;
+        file: File | null;
     }
 }
--- backend/src/common/services/file/types/file.ts
+++
@@ -1,7 +0,0 @@
-import { type File } from 'fastify-multer/lib/interfaces.js';
-
-declare module 'fastify' {
-    interface FastifyRequest {
-        file: File | null;
-    }
-}
