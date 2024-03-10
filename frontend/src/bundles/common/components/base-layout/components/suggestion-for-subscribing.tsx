ac2b957cdc2ffee04e886ed49e80556dde5f0f6d
velisssa <tybinka.vasylyna@gmail.com>
2024-03-10 15:16:40 +0100

    LIME-316: * Editing relative to comments

--- frontend/src/bundles/common/components/base-layout/components/suggestion-for-subscribing.tsx
+++ frontend/src/bundles/common/components/base-layout/components/suggestion-for-subscribing.tsx
@@ -1,7 +1,6 @@
 import { differenceInDays } from 'date-fns';
 
 import { ComponentSize } from '~/bundles/common/enums/component-size.enum.js';
-import { type SubscriptionGetItemResponseDto } from '~/bundles/common/enums/enums.js';
 import { AppRoute } from '~/bundles/common/enums/enums.js';
 import {
     useAppDispatch,
@@ -10,32 +9,34 @@ import {
     useState,
 } from '~/bundles/common/hooks/hooks.js';
 import { actions as subscriptionActions } from '~/bundles/subscription/store/subscriptions.js';
+import { storage, StorageKey } from '~/framework/storage/storage.js';
 
 import { useAppSelector, useCallback } from '../../../hooks/hooks.js';
 import { Button, ButtonVariant, Modal } from '../../components.js';
+import { REMINDER_PERIOD } from '../constants/constants.js';
 
 const SuggestionForSubscribing: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
+    const currentSubscription = useAppSelector(
+        ({ subscriptions }) => subscriptions.currentSubscription,
+    );
 
-    const { currentSubscription } = useAppSelector(({ subscriptions }) => {
-        return {
-            currentSubscription:
-                subscriptions?.currentSubscription as SubscriptionGetItemResponseDto,
-        };
-    });
+    const setLastSuggestionTime = useCallback(async (): Promise<void> => {
+        await storage.set(StorageKey.LAST_SUGGESTION_TIME, String(Date.now()));
+    }, []);
 
-    const handleCloseModal = useCallback((): void => {
-        localStorage.setItem('lastSuggestionTime', String(Date.now()));
+    const handleCloseModal = useCallback(() => {
+        void setLastSuggestionTime();
         setIsModalOpen(false);
-    }, [setIsModalOpen]);
+    }, [setIsModalOpen, setLastSuggestionTime]);
 
-    const handleNavigateToSubscriptionPage = useCallback((): void => {
-        localStorage.setItem('lastSuggestionTime', String(Date.now()));
+    const handleNavigateToSubscriptionPage = useCallback(() => {
+        void setLastSuggestionTime();
         navigate(AppRoute.PROFILE_SUBSCRIPTION);
         setIsModalOpen(false);
-    }, [navigate, setIsModalOpen]);
+    }, [navigate, setIsModalOpen, setLastSuggestionTime]);
 
     const handleLoadCurrentSubscription = useCallback((): void => {
         void dispatch(subscriptionActions.loadCurrentSubscription());
@@ -46,21 +47,30 @@ const SuggestionForSubscribing: React.FC = () => {
     }, [handleLoadCurrentSubscription]);
 
     useEffect(() => {
-        const lastSuggestionTimeString =
-            localStorage.getItem('lastSuggestionTime');
-        const lastSuggestionTime = lastSuggestionTimeString
-            ? Number.parseInt(lastSuggestionTimeString, 10)
-            : null;
-        const numberOfDaysAgo = lastSuggestionTime
-            ? differenceInDays(new Date(), lastSuggestionTime)
-            : 0;
+        const updateStorage = async (): Promise<void> => {
+            if (!(await storage.get(StorageKey.LAST_SUGGESTION_TIME))) {
+                await setLastSuggestionTime();
+            }
+        };
+        void updateStorage();
+    }, [setLastSuggestionTime]);
 
-        if (
-            !currentSubscription &&
-            (!lastSuggestionTime || numberOfDaysAgo >= 7)
-        ) {
-            setIsModalOpen(true);
-        }
+    useEffect(() => {
+        const showModal = async (): Promise<void> => {
+            const lastSuggestionTimeString =
+                (await storage.get(StorageKey.LAST_SUGGESTION_TIME)) || '';
+            const lastSuggestionTime =
+                Number.parseInt(lastSuggestionTimeString, 10) || Date.now();
+            const numberOfDaysAgo = differenceInDays(
+                new Date(),
+                lastSuggestionTime,
+            );
+
+            if (!currentSubscription && numberOfDaysAgo >= REMINDER_PERIOD) {
+                setIsModalOpen(true);
+            }
+        };
+        void showModal();
     }, [currentSubscription]);
 
     return (
