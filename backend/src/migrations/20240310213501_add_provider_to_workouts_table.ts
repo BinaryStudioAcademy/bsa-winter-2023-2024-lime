e5c417bd7a8b03a0e4539e24f2e4cc79be19c8da
Sofiia Trokhymchuk <s.s.trokhymchuk@gmail.com>
2024-03-11 17:24:53 +0200

    LIME-342: * migration

--- backend/src/migrations/20240310213501_add_provider_to_workouts_table.ts
+++ backend/src/migrations/20240310213501_add_provider_to_workouts_table.ts
@@ -6,26 +6,18 @@ const ColumnName = {
     PROVIDER: 'provider',
 };
 
-const providers = ['google-fit', 'strava'];
-const PROVIDER_ENUM_NAME = `${ColumnName.PROVIDER}_enum`;
+const PROVIDER_ENUM_NAME = 'oauth_provider_enum';
 
 async function up(knex: Knex): Promise<void> {
-    return await knex.schema.alterTable(TABLE_NAME, (table) => {
-        table
-            .enum(ColumnName.PROVIDER, providers, {
-                useNative: true,
-                enumName: PROVIDER_ENUM_NAME,
-            })
-            .nullable();
-    });
+    await knex.schema.raw(
+        `ALTER TABLE ${TABLE_NAME} ADD COLUMN ${ColumnName.PROVIDER} ${PROVIDER_ENUM_NAME};`,
+    );
 }
 
 async function down(knex: Knex): Promise<void> {
     await knex.schema.table(TABLE_NAME, (table) => {
         table.dropColumn(ColumnName.PROVIDER);
     });
-
-    await knex.schema.raw(`DROP TYPE IF EXISTS ${PROVIDER_ENUM_NAME}`);
 }
 
 export { down, up };
