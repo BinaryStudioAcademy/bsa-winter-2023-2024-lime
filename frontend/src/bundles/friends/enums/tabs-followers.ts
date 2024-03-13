5842c0ebdacc2b37468f1b4a2c0cefc63828093a
velisssa <tybinka.vasylyna@gmail.com>
2024-03-13 09:31:04 +0100

    LIME-321: * Run lint

--- frontend/src/bundles/friends/enums/tabs-followers.ts
+++ frontend/src/bundles/friends/enums/tabs-followers.ts
@@ -1,7 +1,7 @@
 const TabsFollowers = {
     FIND_THE_FOLLOWERS: 'Find the followers',
     MY_FOLLOWERS: 'My followers',
-    FOLLOWING: 'Following'
+    FOLLOWING: 'Following',
 } as const;
 
 export { TabsFollowers };
