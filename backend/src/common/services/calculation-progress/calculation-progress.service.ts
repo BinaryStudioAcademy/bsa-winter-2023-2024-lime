775f5f49178edab69fe931d3d3193f0dec00e962
valery-chumak <blossssom88@gmail.com>
2024-03-14 18:32:52 +0200

    LIME-406: * add some changes

--- backend/src/common/services/calculation-progress/calculation-progress.service.ts
+++ backend/src/common/services/calculation-progress/calculation-progress.service.ts
@@ -81,10 +81,11 @@ class CalculationProgressService {
         }
 
         if (hasCompletedGoal) {
+            const goalDetails = await this.goalService.find({});
             await this.notificationService.create({
                 userId,
                 title: 'Goal Completed',
-                message: 'Congratulations! You have completed your goal.',
+                message: `Congratulations! You have completed your ${goalDetails?.activityType} goal.`,
                 isRead: false,
                 type: 'default',
             });
