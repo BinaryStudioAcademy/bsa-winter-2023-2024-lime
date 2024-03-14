3b153f2d05d218cc9659ddc9dc49ac62b68f293b
David <davidcharif@hotmail.com>
2024-03-14 17:50:59 -0500

    LIME-244: * Added helper

--- frontend/src/bundles/workouts/helpers/helpers.ts
+++ frontend/src/bundles/workouts/helpers/helpers.ts
@@ -1,2 +1,3 @@
 export { mapWorkoutActivitySelect } from './map-workout-activity-select.helper.js';
 export { mapWorkoutYearSelect } from './map-workout-years-select.helper.js';
+export { sortWorkoutsByDate } from './sort-workouts-by-date.js';
