ff41f8730f75badef03b094ad975da5fe7cef808
David <davidcharif@hotmail.com>
2024-03-15 10:45:12 -0500

    LIME-428: * Prettier

--- frontend/src/bundles/common/components/avatar/avatar.tsx
+++ frontend/src/bundles/common/components/avatar/avatar.tsx
@@ -13,14 +13,14 @@ const sizeToClass: Record<AvatarSize, string> = {
     [ComponentSize.SMALL]: 'h-10 w-10',
     [ComponentSize.MEDIUM]: 'h-20 w-20',
     [ComponentSize.LARGE]: 'h-30 w-30',
-    [ComponentSize.EXTRA_LARGE]: 'h-44 w-44'
+    [ComponentSize.EXTRA_LARGE]: 'h-44 w-44',
 };
 
 const sizeMap: Record<AvatarSize, string> = {
     [ComponentSize.SMALL]: 'text-xl',
     [ComponentSize.MEDIUM]: 'text-3xl',
     [ComponentSize.LARGE]: 'text-4xl',
-    [ComponentSize.EXTRA_LARGE]: 'text-4xl'
+    [ComponentSize.EXTRA_LARGE]: 'text-4xl',
 };
 
 const Avatar = ({ size, email, avatarUrl }: Properties): JSX.Element => {
