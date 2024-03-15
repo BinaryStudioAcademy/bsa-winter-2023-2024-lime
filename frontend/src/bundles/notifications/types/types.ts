7b1fa777a7b5b05c3f0a8c3ffd2f88d796a3d5ba
David <davidcharif@hotmail.com>
2024-03-15 12:08:22 -0500

    LIME-139: * File type rename

--- frontend/src/bundles/notifications/types/types.ts
+++ frontend/src/bundles/notifications/types/types.ts
@@ -1,4 +1,4 @@
-export { type NotificationStateTypeSlice } from './notification-state-type-slice.js';
+export { type NotificationStateTypeSlice } from './notification-state-type-slice.type.js';
 export {
     type NotificationRequestDto,
     type NotificationResponseDto,
