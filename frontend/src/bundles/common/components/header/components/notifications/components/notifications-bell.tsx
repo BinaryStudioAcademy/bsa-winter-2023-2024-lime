f2c37ef3c85fa83e579f0a51ab0aa21ad921b9ee
David <davidcharif@hotmail.com>
2024-03-15 11:31:22 -0500

    LIME-139: - Remove redundant comparison

--- frontend/src/bundles/common/components/header/components/notifications/components/notifications-bell.tsx
+++ frontend/src/bundles/common/components/header/components/notifications/components/notifications-bell.tsx
@@ -19,23 +19,21 @@ const NotificationBell = ({
         <button
             className={getValidClassNames(
                 'relative flex items-center justify-center',
-                animate && showList === false ? 'animate-pulse' : '',
+                animate && !showList ? 'animate-pulse' : '',
             )}
             onClick={onClick}
         >
             <BellIcon
                 className={getValidClassNames(
                     'text-lm-grey-200 w-8 transform transition-all duration-300 ease-in-out sm:w-6',
-                    showList === true ? 'rotate-12 scale-110' : '',
+                    showList ? 'rotate-12 scale-110' : '',
                 )}
             />
             {count > 0 && (
                 <span
                     className={getValidClassNames(
                         'bg-lm-yellow-100 absolute -right-1 -top-2 flex h-full max-h-4 w-full max-w-4 items-center justify-center rounded-full text-xs font-extrabold transition-all ',
-                        showList === true
-                            ? 'bg-lm-yellow-200 -right-0 -top-2'
-                            : '',
+                        showList ? 'bg-lm-yellow-200 -right-0 -top-2' : '',
                     )}
                 >
                     {count}
