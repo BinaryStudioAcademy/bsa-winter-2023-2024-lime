085c94d21286087975396c57387ba1b8214e389f
David <davidcharif@hotmail.com>
2024-03-19 17:01:36 -0500

    LIME-470: * Added max-width, prettier

--- frontend/src/bundles/profile/pages/public-profile-page/public-profile-page.tsx
+++ frontend/src/bundles/profile/pages/public-profile-page/public-profile-page.tsx
@@ -87,7 +87,7 @@ const PublicProfile: React.FC = () => {
 
     if (user) {
         return (
-            <main className="flex h-full w-full sm:w-full sm:flex-col md:flex-row-reverse">
+            <main className="flex h-full w-full max-w-[1136px] sm:w-full sm:flex-col md:flex-row-reverse xl:flex xl:gap-8 2xl:basis-[1136px]">
                 <div className="sm:mb-8">
                     <PersonalDetails
                         id={NumericId}
