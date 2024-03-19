185d4c895d02e014edd6ad2b2b6dc30f0fddd484
David <davidcharif@hotmail.com>
2024-03-19 06:42:27 -0500

    LIME-463: * Prettier

--- frontend/src/bundles/workouts/components/workout-stats/workout-stats.tsx
+++ frontend/src/bundles/workouts/components/workout-stats/workout-stats.tsx
@@ -23,7 +23,8 @@ const WorkoutStats: React.FC<Properties> = ({ workout }): JSX.Element => {
         workoutEndedAt,
     } = workout;
 
-    const workoutDistance = steps && steps != 0 ? steps :  metersToKilometers(distance);
+    const workoutDistance =
+        steps && steps != 0 ? steps : metersToKilometers(distance);
     const distanceUnit = steps
         ? capitalizeFirstLetter(WorkoutUnit.STEPS)
         : WorkoutUnit.KILOMETERS;
