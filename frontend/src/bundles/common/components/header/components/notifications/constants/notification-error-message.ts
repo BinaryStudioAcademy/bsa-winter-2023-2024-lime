9c63bc10f2b3357b68992e3d1b70be9d35b50140
David <davidcharif@hotmail.com>
2024-03-19 07:18:34 -0500

    LIME-465: * Prettier

--- frontend/src/bundles/common/components/header/components/notifications/constants/notification-error-message.ts
+++ frontend/src/bundles/common/components/header/components/notifications/constants/notification-error-message.ts
@@ -1,3 +1,4 @@
-const NOTIFICATION_ERROR_MESSAGE = 'Error while fetching notifications. Please try again later.';
+const NOTIFICATION_ERROR_MESSAGE =
+    'Error while fetching notifications. Please try again later.';
 
 export { NOTIFICATION_ERROR_MESSAGE };
