434db76af39aa423a010042164864492a76d0565
David <davidcharif@hotmail.com>
2024-03-20 09:12:07 -0500

    LIME-244: * Prettier

--- frontend/src/bundles/workouts/helpers/format-workout-data-date.helper.ts
+++ frontend/src/bundles/workouts/helpers/format-workout-data-date.helper.ts
@@ -1,8 +1,10 @@
 import { type WorkoutResponseDto } from '../types/types.js';
 
-const formatWorkoutDataDate = (workoutData: WorkoutResponseDto[]): WorkoutResponseDto[] => {
+const formatWorkoutDataDate = (
+    workoutData: WorkoutResponseDto[],
+): WorkoutResponseDto[] => {
     return workoutData.map((workout) => {
-        const date = new Date(workout.workoutStartedAt);       
+        const date = new Date(workout.workoutStartedAt);
         return { ...workout, workoutStartedAt: date };
     });
 };
