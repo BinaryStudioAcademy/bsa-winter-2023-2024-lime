b973610da272fda48075fbf97ae5e1ef6190a494
David <davidcharif@hotmail.com>
2024-03-20 11:15:43 -0500

    LIME-244: * Simplify type

--- frontend/src/bundles/workouts/components/sub-navigation/components/sub-navigation-list.tsx
+++ frontend/src/bundles/workouts/components/sub-navigation/components/sub-navigation-list.tsx
@@ -1,12 +1,10 @@
-import { type WorkoutResponseDto } from 'shared';
+import { type WorkoutGetAllResponseDto } from '~/bundles/workouts/types/types.js';
 
 import { SubNavigationItemWorkout } from './components.js';
 
-type Properties = {
-    items: WorkoutResponseDto[];
-};
-
-const SubNavigationWorkoutsList = ({ items }: Properties): JSX.Element => {
+const SubNavigationWorkoutsList = ({
+    items,
+}: WorkoutGetAllResponseDto): JSX.Element => {
     return (
         <div className="max-h-[15rem] overflow-y-scroll sm:h-[10rem]  sm:max-h-[10rem] md:h-[90%] md:max-h-full lg:h-fit lg:max-h-[30rem]">
             <ul className="flex h-full flex-col sm:gap-2 md:gap-3">
