a8fecaf79e8a90a88b6025e8a214fd34ee9721fd
Artem Lymarenko <artemchiklym@gmail.com>
2024-03-21 18:41:37 +0200

    LIME-518: + search chats functionality

---
+++ frontend/src/bundles/chats/helpers/filter-chats.helper.ts
@@ -0,0 +1,20 @@
+import { type ChatPreviewResponseDto } from '../types/types.js';
+
+const filterChats = (
+    chats: ChatPreviewResponseDto[],
+    search: string,
+): ChatPreviewResponseDto[] => {
+    return chats.filter((chat) =>
+        chat.users.some(
+            ({ fullName, email }) =>
+                (fullName &&
+                    fullName
+                        .toLowerCase()
+                        .includes(search.trim().toLowerCase())) ||
+                (email &&
+                    email.toLowerCase().includes(search.trim().toLowerCase())),
+        ),
+    );
+};
+
+export { filterChats };
