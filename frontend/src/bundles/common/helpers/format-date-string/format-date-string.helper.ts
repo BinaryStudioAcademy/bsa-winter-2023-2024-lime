5c727ac1323cecc211877252e6ca6c18d14e4829
Sergii Ternytskyi <serhij86@gmail.com>
2024-03-21 19:52:48 +0200

    LIME-517: + add data to notifications

--- frontend/src/bundles/common/helpers/format-date-string/format-date-string.helper.ts
+++ frontend/src/bundles/common/helpers/format-date-string/format-date-string.helper.ts
@@ -2,10 +2,13 @@ import { DateObject } from 'react-multi-date-picker';
 
 import { DateFormat } from '~/bundles/common/enums/enums.js';
 
-const formatDateString = (date: Date): string => {
+const formatDateString = (
+    date: Date,
+    format: string = DateFormat.FULL_DAY_DATE_TIME,
+): string => {
     const convertedDate = new DateObject(date);
 
-    return convertedDate.format(DateFormat.FULL_DAY_DATE_TIME);
+    return convertedDate.format(format);
 };
 
 export { formatDateString };
